863a61b76d673fa9ff58dcb8a92a6eeecb78f212
Jon Snyder <josnyder@adobe.com>
2019-03-01 09:17:14 -0700

    Add an intro in the rollup config to check for unsupported browsers

--- rollup.config.js
+++ rollup.config.js
@@ -8,12 +8,17 @@ export default {
     {
       file: "dist/atag.js",
       format: "umd",
-      name: "atag"
+      name: "atag",
+      // Allow non-IE browsers and IE10 and IE11
+      // document.documentMode was added in IE8, and is specific to IE.
+      // IE7 and lower are not ES5 compatible so will get a parse error loading the library.
+      intro: "if (document.documentMode && document.documentMode < 10) { console.warn('The Adobe Experience Cloud Web SDK does not support IE 9 and below.'); return; }"
     },
     {
       file: "sandbox/public/atag.js",
       format: "umd",
-      name: "atag"
+      name: "atag",
+      intro: "if (document.documentMode && document.documentMode < 10) { console.warn('The Adobe Experience Cloud Web SDK does not support IE 9 and below.'); return; }"
     }
   ],
   plugins: [
