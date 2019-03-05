0839a7ceb8033f239dfb02dadf8b38b77ddf2d04
Artur Ciocanu <ciocanu@adobe.com>
2019-03-05 08:06:44 -0800

    CORE-26016 - ensure that personalization content is rendered as soon … (#14)
    * CORE-26016 - ensure that personalization content is rendered as soon as possible

--- src/components/Personalization/index.js
+++ src/components/Personalization/index.js
@@ -1,5 +1,105 @@
+import document from "@adobe/reactor-document";
+
+const KEY_PREFIX = "___adbe";
+const KEY_DETECT_PREFIX = `${KEY_PREFIX}-detect`;
+
+function hash(string) {
+  let result = 0;
+  const { length } = string;
+
+  for (let i = 0; i < length; i += 1) {
+    /* eslint-disable */
+    result = ((result << 5) - result + string.charCodeAt(i)) & 0xffffffff;
+    /* eslint-enable */
+  }
+
+  return result;
+}
+
+function buildKey(prefix, selector) {
+  return `${prefix}-${hash(selector)}`;
+}
+
+function createStyleTag(className, content) {
+  const style = document.createElement("style");
+  style.className = className;
+  style.innerHTML = content;
+
+  return style;
+}
+
+function appendTo(parent, element) {
+  parent.appendChild(element);
+}
+
+function removeFrom(parent, element) {
+  parent.removeChild(element);
+}
+
+function setupElementDetection(key, selector, callback) {
+  const content = `
+    @keyframes ${key} {  
+      from { opacity: 0.99; }
+      to { opacity: 1; }  
+    }
+
+    ${selector} {
+      animation-duration: 0.001s;
+      animation-name: ${key};
+    }
+    `;
+
+  document.addEventListener("animationstart", callback, false);
+  appendTo(document.head, createStyleTag(key, content));
+}
+
+function hideElement(selector) {
+  const key = buildKey(KEY_PREFIX, selector);
+  const content = `${selector} { visibility: hidden }`;
+
+  appendTo(document.head, createStyleTag(key, content));
+}
+
+function showElement(selector) {
+  const key = buildKey(KEY_PREFIX, selector);
+  const elements = document.querySelectorAll(`.${key}`);
+
+  elements.forEach(e => removeFrom(document.head, e));
+}
+
+function render(cache, event) {
+  const { animationName } = event;
+
+  if (animationName.indexOf(KEY_DETECT_PREFIX) === -1) {
+    return;
+  }
+
+  const option = cache[animationName];
+
+  if (!option) {
+    console.log("Element with key:", animationName, "not in cache");
+    return;
+  }
+
+  const { type, selector, content } = option;
+
+  switch (type) {
+    case "setHtml":
+      /* eslint-disable */
+      event.target.innerHTML = content;
+      /* eslint-enable */
+
+      showElement(selector);
+      break;
+    default:
+      console.log(type, "rendeing is not supported");
+      break;
+  }
+}
+
 export default () => {
   let core;
+  const storage = {};
 
   const collect = offerInfo => {
     const tracker = core.components.getByNamespace("Tracker");
@@ -14,6 +114,7 @@ export default () => {
       },
       onBeforeViewStart(payload) {
         console.log("Personalization:::onBeforeViewStart");
+
         payload.appendToQuery({
           personalization: {
             sessionId: "1234235"
@@ -23,16 +124,17 @@ export default () => {
       onViewStartResponse({ resources: { personalization = [] } } = {}) {
         console.log("Personalization:::onViewStartResponse");
 
-        document.addEventListener("DOMContentLoaded", () => {
-          personalization.forEach(offer => {
-            const el = document.querySelector(offer.offerMboxSelector);
-            if (el) {
-              el.innerHTML = offer.offerHtmlPayload;
-              collect({
-                event: "offer-rendered",
-                ...offer
-              });
-            }
+        // Caution!!! Here comes Target black magic
+
+        personalization.forEach(option => {
+          const { selector, eventToken } = option;
+          const key = buildKey(KEY_DETECT_PREFIX, selector);
+          storage[key] = option;
+
+          hideElement(selector);
+          setupElementDetection(key, selector, event => {
+            render(storage, event);
+            collect({ eventToken });
           });
         });
       }
