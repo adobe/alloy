4894de89d910617e60a305c7e06d0e7113db3201
Joe Khoury <khoury@adobe.com>
2019-04-04 12:25:08 -0400

    Change browser to chrome.

--- karma.conf.js
+++ karma.conf.js
@@ -81,6 +81,14 @@ module.exports = config => {
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
+    customLaunchers: {
+        Chrome: {
+            base: 'Chrome',
+            flags: [
+                '--no-sandbox'
+            ]
+        }
+    },
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
