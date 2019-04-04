eb90ac2ff81067da9b5a4f5b284b1eebf1db873d
Joe Khoury <khoury@adobe.com>
2019-04-04 12:32:20 -0400

    Change browser to chrome 2.

--- karma.conf.js
+++ karma.conf.js
@@ -9,8 +9,8 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-const puppeteer = require('puppeteer');
-process.env.CHROME_BIN = puppeteer.executablePath();
+// const puppeteer = require('puppeteer');
+// process.env.CHROME_BIN = puppeteer.executablePath();
 const path = require("path");
 
 const reporters = ["spec", "coverage"];
