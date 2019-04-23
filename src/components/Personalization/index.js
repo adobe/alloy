ac569ebc0b8841186f88668edc4f9b84abb48783
Aaron Hardy <aahardy@adobe.com>
2019-04-23 09:46:31 -0600

    Re-enable personalization response handling.

--- src/components/Personalization/index.js
+++ src/components/Personalization/index.js
@@ -149,9 +149,6 @@ const createPersonalization = ({ logger }) => {
       //  Jon's network gateway work
 
       onResponse(response) {
-        // TODO: pull personalization data from response fragments, which requires
-        //  Jon's network gateway work
-        return;
         // eslint-disable-next-line no-unreachable
         console.log("Personalization:::onResponse");
         const personalization =
