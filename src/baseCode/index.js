5539bb4e52da784ca5a4cbe3a7dc97dbf9f4c99d
Aaron Hardy <aahardy@adobe.com>
2019-05-01 09:41:52 -0600

    Added comments to base code.

--- src/baseCode/index.js
+++ src/baseCode/index.js
@@ -15,13 +15,42 @@ governing permissions and limitations under the License.
 /* eslint prefer-rest-params: 0 */
 /* eslint no-var: 0 */
 
+/**
+ * This is the code, once minified, that an Alloy consumer copies and pasted
+ * into their website's HTML. It sets up any global functions the consumer will
+ * use to interact with Alloy. It also provides a queuing mechanism whereby
+ * any function calls made by the consumer before the Alloy library file
+ * loads will be enqueued and later processed by Alloy once the Alloy library
+ * file has loaded. Because each call may be asynchronously processed, a promise
+ * will always be returned to the consumer any time the consumer attempts to
+ * execute a command.
+ *
+ * Along with this base code, a script tag (not shown here) will also be added
+ * to the consumer website's HTML that will load the Alloy library file.
+ *
+ * Take particular care to write well-minifiable code, as consumers are
+ * particularly sensitive to base code size.
+ */
+
 (function(window, instanceNames) {
   instanceNames.forEach(function(instanceNamespace) {
     if (!window[instanceNamespace]) {
+      // __alloyNS stores a name of each "instance", or in other words, each
+      // global function created that the consumer will use. This array is
+      // what the Alloy library will consult once it is loaded to determine
+      // which global functions have been set up so that is can connect them to
+      // the library's command processing pipeline.
       (window.__alloyNS = window.__alloyNS || []).push(instanceNamespace);
       window[instanceNamespace] = function() {
         var userProvidedArgs = arguments;
+        // Always return a promise, because the command may be executed
+        // asynchronously, especially if the Alloy library has not yet loaded.
         return new Promise(function(resolve, reject) {
+          // Push required call information into the queue. Once the Alloy
+          // library is loaded, it will process this queue and resolve/reject
+          // the promise we just returned to the consumer. If the Alloy
+          // library has already loaded, then it will have alredy overridden
+          // q.push and will therefore process the call immediately.
           window[instanceNamespace].q.push([resolve, reject, userProvidedArgs]);
         });
       };
