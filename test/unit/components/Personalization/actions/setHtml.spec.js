165cea5cdde1f59923eb82a2fcecdc6da4627bac
Artur Ciocanu <ciocanu@adobe.com>
2019-06-17 16:10:24 -0700

    Trying to fix unit tests take 2

--- test/unit/components/Personalization/actions/setHtml.spec.js
+++ test/unit/components/Personalization/actions/setHtml.spec.js
@@ -6,27 +6,34 @@ import {
 } from "../../../../../src/utils/dom";
 import createSetHtml from "../../../../../src/components/Personalization/actions/setHtml";
 
+const cleanUp = () => {
+  selectNodes("div#setHtml").forEach(removeNode);
+  selectNodes("style").forEach(node => {
+    if (node.innerText.indexOf("setHtml") !== -1) {
+      removeNode(node);
+    }
+  });
+};
+
 describe("Presonalization::actions::setHtml", () => {
   beforeEach(() => {
-    selectNodes("div").forEach(removeNode);
-    selectNodes("style").forEach(removeNode);
+    cleanUp();
   });
 
   afterEach(() => {
-    selectNodes("div").forEach(removeNode);
-    selectNodes("style").forEach(removeNode);
+    cleanUp();
   });
 
   it("should set personalized content", () => {
     const collect = jasmine.createSpy();
     const setHtml = createSetHtml(collect);
-    const element = createNode("div", { id: "foo" });
+    const element = createNode("div", { id: "setHtml" });
     element.innerText = "foo";
 
     appendNode(document.body, element);
 
     const settings = { content: "bar", meta: { a: 1 } };
-    const event = { element, prehidingSelector: "#foo" };
+    const event = { element, prehidingSelector: "#setHtml" };
 
     setHtml(settings, event);
 
