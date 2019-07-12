1837f627ee3bab2a358e337255068703bb546321
alshaw <alshaw@adobe.com>
2019-07-12 14:43:55 -0600

    Changed non-programatic text back to ID

--- sandbox/src/OrgTwo.js
+++ sandbox/src/OrgTwo.js
@@ -4,8 +4,8 @@ export default function OrgTwo() {
     return (
         <div>
         <h2>Organization Two</h2>
-        <h3>This view is managed by a partnering company that owns a different Org Id.</h3>
-        <p>For that reason, we have created a second instance of Alloy, and configurered it using the Org and Property Ids of Organization Two.</p>
+        <h3>This view is managed by a partnering company that owns a different Org ID.</h3>
+        <p>For that reason, we have created a second instance of Alloy, and configurered it using the Org and Property IDs of Organization Two.</p>
         <p>Alloy instance is called: <i>organizationTwo</i></p>
         <pre>
             <code>
