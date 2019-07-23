8ad0134342aeef33a157e20e36b033cff5728aba
Artur Ciocanu <ciocanu@adobe.com>
2019-07-23 13:00:29 +0300

    Fixing comments

--- src/components/Personalization/actions/setAttribute.js
+++ src/components/Personalization/actions/setAttribute.js
@@ -23,7 +23,7 @@ export default collect => {
       });
     });
 
-    // after rendering we should show remove the flicker control styles
+    // after rendering we should remove the flicker control styles
     showElements(prehidingSelector);
 
     // make sure we send back the metadata after successful rendering
