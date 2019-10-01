2eb467b9ab15d72f6a72777de50e52be1b153c5c
Aaron Hardy <aahardy@adobe.com>
2019-10-01 13:01:25 -0600

    Skip sendBeaconFactory tests in browsers that don't support sendBeacon.

--- test/unit/specs/core/network/sendBeaconFactory.spec.js
+++ test/unit/specs/core/network/sendBeaconFactory.spec.js
@@ -12,39 +12,47 @@ governing permissions and limitations under the License.
 
 import sendBeaconFactory from "../../../../../src/core/network/sendBeaconFactory";
 
-describe("sendBeaconFactory", () => {
-  it("falls back to fetch if sendBeacon fails", () => {
-    const navigator = {
-      sendBeacon: jasmine.createSpy().and.returnValue(false)
-    };
-    const fetchPromise = Promise.resolve();
-    const fetch = jasmine.createSpy().and.returnValue(fetchPromise);
-    const logger = {
-      log: jasmine.createSpy()
-    };
-    const sendBeacon = sendBeaconFactory(navigator, fetch, logger);
-    const body = { a: "b" };
-    const result = sendBeacon("https://example.com/endpoint", body);
-    expect(navigator.sendBeacon).toHaveBeenCalledWith(
-      "https://example.com/endpoint",
-      jasmine.any(Object)
-    );
-    expect(fetch).toHaveBeenCalledWith("https://example.com/endpoint", body);
-    expect(logger.log).toHaveBeenCalledWith(
-      jasmine.stringMatching("call has failed")
-    );
-    expect(result).toBe(fetchPromise);
-  });
+// When running these tests in IE 11, they fail because IE doesn't like the
+// way the blob is constructed (see
+// https://github.com/bpampuch/pdfmake/pull/297/files for a workaround).
+// Fortunately, if navigator.sendBeacon doesn't exist (IE 11), sendBeaconFactory
+// should never be used (see createNetworkStrategy.js), so we can skip
+// these tests altogether.
+if (window.navigator.sendBeacon) {
+  describe("sendBeaconFactory", () => {
+    it("falls back to fetch if sendBeacon fails", () => {
+      const navigator = {
+        sendBeacon: jasmine.createSpy().and.returnValue(false)
+      };
+      const fetchPromise = Promise.resolve();
+      const fetch = jasmine.createSpy().and.returnValue(fetchPromise);
+      const logger = {
+        log: jasmine.createSpy()
+      };
+      const sendBeacon = sendBeaconFactory(navigator, fetch, logger);
+      const body = { a: "b" };
+      const result = sendBeacon("https://example.com/endpoint", body);
+      expect(navigator.sendBeacon).toHaveBeenCalledWith(
+        "https://example.com/endpoint",
+        jasmine.any(Object)
+      );
+      expect(fetch).toHaveBeenCalledWith("https://example.com/endpoint", body);
+      expect(logger.log).toHaveBeenCalledWith(
+        jasmine.stringMatching("call has failed")
+      );
+      expect(result).toBe(fetchPromise);
+    });
 
-  it("does not fall back to fetch if sendBeacon succeeds", () => {
-    const navigator = {
-      sendBeacon: jasmine.createSpy().and.returnValue(true)
-    };
-    const body = { a: "b" };
-    const fetch = jasmine.createSpy();
-    const sendBeacon = sendBeaconFactory(navigator, fetch);
-    return sendBeacon("https://example.com/endpoint", body).then(() => {
-      expect(fetch).not.toHaveBeenCalled();
+    it("does not fall back to fetch if sendBeacon succeeds", () => {
+      const navigator = {
+        sendBeacon: jasmine.createSpy().and.returnValue(true)
+      };
+      const body = { a: "b" };
+      const fetch = jasmine.createSpy();
+      const sendBeacon = sendBeaconFactory(navigator, fetch);
+      return sendBeacon("https://example.com/endpoint", body).then(() => {
+        expect(fetch).not.toHaveBeenCalled();
+      });
     });
   });
-});
+}
