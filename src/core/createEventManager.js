19b6b9add82548dbfb7f9a6b44fbc7bdced2e31c
Joe Khoury <jfkhoury@gmail.com>
2019-10-29 19:03:39 -0400

    Minor updates to sandbox.
    Enable AEP sync validation by default for now. We should support a  config that should enable this feature and more debugging specific stuff.

--- src/core/createEventManager.js
+++ src/core/createEventManager.js
@@ -37,6 +37,10 @@ export default ({ createEvent, optIn, lifecycle, network, config }) => {
       payload.mergeMeta({
         gateway: {
           imsOrgId
+        },
+        // TODO: We should connect this to a new `debug` config.
+        collect: {
+          synchronousValidation: true
         }
       });
 
