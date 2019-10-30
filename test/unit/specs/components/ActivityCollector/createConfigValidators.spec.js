dcb4170709260ef0b6506e46b39da4edc4cd1bae
Martin Liljenback <liljenback@gmail.com>
2019-10-30 12:38:27 -0700

    CORE-36750 Unit tests for ActivityCollector configuration validators

--- test/unit/specs/components/ActivityCollector/createConfigValidators.spec.js
+++ test/unit/specs/components/ActivityCollector/createConfigValidators.spec.js
@@ -10,5 +10,49 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-// eslint-disable-next-line no-unused-vars
 import createConfigValidators from "../../../../../src/components/ActivityCollector/createConfigValidators";
+import createConfig from "../../../../../src/core/createConfig";
+
+describe("ActivityCollector::createConfigValidators", () => {
+  [
+    {},
+    {
+      clickCollectionEnabled: false
+    },
+    {
+      clickCollectionEnabled: false,
+      downloadLinkQualifier: ""
+    }
+  ].forEach((cfg, i) => {
+    it(`validates configuration (${i})`, () => {
+      const configObj = createConfig(cfg);
+      configObj.addValidators(createConfigValidators());
+      configObj.validate();
+    });
+  });
+
+  [
+    { clickCollectionEnabled: "" },
+    {
+      clickCollectionEnabled: true,
+      downloadLinkQualifier: "["
+    }
+  ].forEach((cfg, i) => {
+    it(`invalidates configuration (${i})`, () => {
+      const configObj = createConfig(cfg);
+      configObj.addValidators(createConfigValidators());
+      expect(() => {
+        configObj.validate();
+      }).toThrowError();
+    });
+  });
+
+  ["clickCollectionEnabled", "downloadLinkQualifier"].forEach((cfgKey, i) => {
+    it(`add default configuration key (${i})`, () => {
+      const configObj = createConfig({});
+      configObj.addValidators(createConfigValidators());
+      configObj.validate();
+      expect(configObj[cfgKey]).toBeDefined();
+    });
+  });
+});
