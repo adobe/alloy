869461e07bfd1e9d24193ee9fcdcfd15b8a830c0
Shamiul Mowla <mowla@adobe.com>
2019-11-09 11:20:28 -0500

    Reset head before pulling

--- scripts/deploy_sandbox.sh
+++ scripts/deploy_sandbox.sh
@@ -1,1 +1,1 @@
-ssh -t mowla@20.186.4.13 "cd /var/www/alloyio.com && git pull origin master && npm i && npm run build:prod && npm run sandbox:install && npm run sandbox:build && cp /var/www/alloyio.com/dist/standalone/alloy.js /var/www/alloyio.com/sandbox/build"
+ssh -t mowla@20.186.4.13 "cd /var/www/alloyio.com && git reset HEAD --hard && git pull origin master && npm i && npm run build:prod && npm run sandbox:install && npm run sandbox:build && cp /var/www/alloyio.com/dist/standalone/alloy.js /var/www/alloyio.com/sandbox/build"
