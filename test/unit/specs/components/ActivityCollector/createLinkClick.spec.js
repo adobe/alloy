f8fef55f40d993fcadb82e5dd7f2c2b8e5a8a3fd
Martin Liljenback <liljenback@gmail.com>
2019-11-11 19:22:43 -0800

    Link click unit test

--- test/unit/specs/components/ActivityCollector/createLinkClick.spec.js
+++ test/unit/specs/components/ActivityCollector/createLinkClick.spec.js
@@ -10,5 +10,40 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-// eslint-disable-next-line no-unused-vars
 import createLinkClick from "../../../../../src/components/ActivityCollector/createLinkClick";
+import createEvent from "../../../../../src/core/createEvent";
+import createConfigValidators from "../../../../../src/components/ActivityCollector/createConfigValidators";
+
+describe("ActivityCollector::createLinkClick", () => {
+  const cfgValidators = createConfigValidators();
+  const config = {
+    downloadLinkQualifier: cfgValidators.downloadLinkQualifier.defaultValue
+  };
+  const mockWindow = {
+    location: {
+      protocol: "https:",
+      host: "example.com",
+      hostname: "example.com",
+      pathname: "/"
+    }
+  };
+  const supportedLinkElement = {
+    tagName: "A",
+    href: "index.html"
+  };
+  const unsupportedLinkElement = {
+    tagName: "LINK",
+    href: "index.html"
+  };
+  const linkClick = createLinkClick(mockWindow, config);
+  it("Extends event XDM data with link information for supported anchor elements", () => {
+    const event = createEvent();
+    linkClick(event, supportedLinkElement);
+    expect(event.isEmpty()).toBe(false);
+  });
+  it("Does not extend event XDM data with link information for unsupported anchor elements", () => {
+    const event = createEvent();
+    linkClick(event, unsupportedLinkElement);
+    expect(event.isEmpty()).toBe(true);
+  });
+});
