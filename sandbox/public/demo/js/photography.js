e1dec311a9b44cb8d2ff615a0b5e4004d1c13a66
Joe Khoury <jfkhoury@gmail.com>
2019-11-14 10:49:31 -0500

    Add a photography demo site.

---
+++ sandbox/public/demo/js/photography.js
@@ -0,0 +1,27 @@
+"use strict";
+
+window.addEventListener('load', function() {
+
+
+
+
+//------------------------------------------------------------------------
+//						OWL CAROUSEL OPTIONS
+//------------------------------------------------------------------------
+
+$('.carousel-single').owlCarousel({
+    loop: false,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoHeight: false,
+    items: 1,
+    dots: true,
+    navText: ['',''],
+    rewind: true
+});
+
+
+
+});
