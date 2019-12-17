f3c67e23cb6ff513c9333d3284ea4cbd54c9139d
Martin Liljenback <liljenback@gmail.com>
2019-12-17 12:24:07 -0800

    CORE-39046 Shortened documentation links

--- src/components/DataCollector/index.js
+++ src/components/DataCollector/index.js
@@ -12,8 +12,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const CONFIG_DOC_URI =
-  "https://www.adobe.io/apis/experienceplatform/home/services/web-sdk.html#!api-specification/markdown/narrative/edge/fundamentals/executing-commands.md";
+const CONFIG_DOC_URI = "https://adobe.ly/2r0uUjh";
 
 const createDataCollector = ({ eventManager, logger }) => {
   return {
