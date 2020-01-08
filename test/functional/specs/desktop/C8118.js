e6bb5222971b26b43b60b77d505ac30367cc297c
Joe Khoury <jfkhoury@gmail.com>
2020-01-07 22:01:19 -0500

    Fix test C8118

--- test/functional/specs/desktop/C8118.js
+++ test/functional/specs/desktop/C8118.js
@@ -1,30 +1,30 @@
-// import { t, Selector } from "testcafe";
-// import createNetworkLogger from "../../helpers/networkLogger";
-// import { responseStatus } from "../../helpers/assertions/index";
-// import testServerUrl from "../../helpers/constants/testServerUrl";
+import { t, Selector } from "testcafe";
+import createNetworkLogger from "../../helpers/networkLogger";
+import { responseStatus } from "../../helpers/assertions/index";
+import testServerUrl from "../../helpers/constants/testServerUrl";
 
-// const linkPageWithClickHandler = `${testServerUrl}/test/functional/sandbox/html/linkPageWithClickHandler.html`;
+const linkPageWithClickHandler = `${testServerUrl}/test/functional/sandbox/html/linkPageWithClickHandler.html`;
 
-// const networkLogger = createNetworkLogger();
+const networkLogger = createNetworkLogger();
 
-// fixture`C8118`
-//   .page(linkPageWithClickHandler)
-//   .requestHooks(
-//     networkLogger.gatewayEndpointLogs,
-//     networkLogger.sandboxEndpointLogs
-//   );
+fixture`C8118`
+  .page(linkPageWithClickHandler)
+  .requestHooks(
+    networkLogger.edgeEndpointLogs,
+    networkLogger.sandboxEndpointLogs
+  );
 
-// test.meta({
-//   ID: "C8118",
-//   SEVERITY: "P0",
-//   TEST_RUN: "Regression"
-// });
+test.meta({
+  ID: "C8118",
+  SEVERITY: "P0",
+  TEST_RUN: "Regression"
+});
 
-// test("Regression: Load page with link. Click link. Verify request.", async () => {
-//   await t.click(Selector("#alloy-link-test"));
-//   await responseStatus(networkLogger.gatewayEndpointLogs.requests, 200);
-//   const gatewayRequest = networkLogger.gatewayEndpointLogs.requests[0];
-//   const requestBody = JSON.parse(gatewayRequest.request.body);
-//   const destinationUrl = requestBody.events[0].xdm.web.webinteraction.URL;
-//   await t.expect(destinationUrl).contains("missing.html");
-// });
+test("Test C8118: Load page with link. Click link. Verify request.", async () => {
+  await t.click(Selector("#alloy-link-test"));
+  await responseStatus(networkLogger.edgeEndpointLogs.requests, 204);
+  const gatewayRequest = networkLogger.edgeEndpointLogs.requests[0];
+  const requestBody = JSON.parse(gatewayRequest.request.body);
+  const destinationUrl = requestBody.events[0].xdm.web.webInteraction.URL;
+  await t.expect(destinationUrl).contains("missing.html");
+});
