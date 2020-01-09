cb43f1d97389ce98908733c3a5415f1605b2532b
Shamiul Mowla <sham.mowla@gmail.com>
2020-01-09 13:14:58 -0500

    Added pages to test cookies in first party and third party context.

---
+++ test/functional/sandbox/html/_testbed/third-party.js
@@ -0,0 +1,24 @@
+/* eslint-disable func-style */
+function setExpire() {
+  const now = new Date();
+  const time = now.getTime();
+  const expireTime = time + 864000;
+  now.setTime(expireTime);
+  return `;expires=${now.toGMTString()}`;
+}
+
+function setPath() {
+  return ";path=/";
+}
+
+function updateCookie() {
+  const oldcookie = document.cookie;
+  const val = "1234567890";
+  console.log(`read cookie: ${oldcookie}`);
+  document.cookie = `thirdpartytest=${encodeURIComponent(
+    val
+  )}${setExpire()}${setPath()}`;
+  console.log(`updating cookie to:${document.cookie}`);
+}
+
+updateCookie();
