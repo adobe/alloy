c2363c4397d93defef7d1fb1193a683cdf47e07b
Shamiul Mowla <sham.mowla@gmail.com>
2020-01-14 15:57:26 -0500

    C2595 and updated paths for C12411.

--- test/functional/specs/desktop/C2595.js
+++ test/functional/specs/desktop/C2595.js
@@ -8,19 +8,19 @@ const urlCollector = `${testServerUrl}/test/functional/sandbox/html/alloySdk.htm
 const networkLogger = createNetworkLogger();
 
 fixtureFactory({
-  title: "C12411: Requests destinations if configured",
+  title: "C2595: Event command passes the org ID on the request",
   url: urlCollector,
   requestHooks: [networkLogger.edgeEndpointLogs]
 });
 
 test.meta({
-  ID: "C12411",
+  ID: "C2595",
   SEVERITY: "P0",
   TEST_RUN: "Regression"
 });
 
-test("Test C12411: Requests destinations if configured.", async () => {
-  await t.click(Selector("#destinationsEnabled-button"));
+test("Test C2595: Event command passes the org ID on the request.", async () => {
+  await t.click(Selector("#debugEnabled-button"));
   await t.click(Selector("#event-button"));
 
   await responseStatus(networkLogger.edgeEndpointLogs.requests, 200);
@@ -28,10 +28,5 @@ test("Test C12411: Requests destinations if configured.", async () => {
   await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(1);
 
   const request = networkLogger.edgeEndpointLogs.requests[0].request.body;
-
-  await t
-    .expect(request)
-    .contains(
-      '"activation":{"urlDestinationsEnabled":true,"storedDestinationsEnabled":true}'
-    );
+  await t.expect(request).contains("orgId");
 });
