43eb6b712bec45b19ab8dc3dd3edbe941ead9791
Shamiul Mowla <sham.mowla@gmail.com>
2020-01-16 12:18:05 -0500

    C2595 and updated paths for C12411. (#332)
    * C2595 and updated paths for C12411.
    * Assert nested edge request object for imsOrg.

--- test/functional/specs/desktop/C2595.js
+++ test/functional/specs/desktop/C2595.js
@@ -8,30 +8,26 @@ const urlCollector = `${testServerUrl}/test/functional/sandbox/html/alloySdk.htm
 const networkLogger = createNetworkLogger();
 
 fixtureFactory({
-  title: "C12411: Requests destinations if configured",
+  title: "C2595: Event command passes the org ID on the request",
   url: urlCollector,
   requestHooks: [networkLogger.edgeEndpointLogs]
 });
 
 test.meta({
-  ID: "C12411",
+  ID: "C2595",
   SEVERITY: "P0",
   TEST_RUN: "Regression"
 });
 
-test("Test C12411: Requests destinations if configured.", async () => {
-  await t.click(Selector("#destinationsEnabled-button"));
+test("Test C2595: Event command passes the org ID on the request.", async () => {
+  await t.click(Selector("#debugEnabled-button"));
   await t.click(Selector("#event-button"));
 
   await responseStatus(networkLogger.edgeEndpointLogs.requests, 200);
-
   await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(1);
 
   const request = networkLogger.edgeEndpointLogs.requests[0].request.body;
+  const stringifyRequest = JSON.parse(request);
 
-  await t
-    .expect(request)
-    .contains(
-      '"activation":{"urlDestinationsEnabled":true,"storedDestinationsEnabled":true}'
-    );
+  await t.expect(stringifyRequest.meta.configOverrides.orgId).ok();
 });
