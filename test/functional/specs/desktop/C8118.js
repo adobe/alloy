365484179b3080bdaf973d9a3b18bf9fd28aecaf
mliljenb <mliljenb@adobe.com>
2020-01-17 10:16:59 -0800

    Updating click activity functional test to use a working link and independent network logger

--- test/functional/specs/desktop/C8118.js
+++ test/functional/specs/desktop/C8118.js
@@ -1,17 +1,17 @@
-import { t, Selector } from "testcafe";
-import createNetworkLogger from "../../helpers/networkLogger";
-import { responseStatus } from "../../helpers/assertions/index";
+import { t, Selector, RequestLogger } from "testcafe";
 import testServerUrl from "../../helpers/constants/testServerUrl";
 import fixtureFactory from "../../helpers/fixtureFactory";
 
 const linkPageWithClickHandler = `${testServerUrl}/test/functional/sandbox/html/linkPageWithClickHandler.html`;
 
-const networkLogger = createNetworkLogger();
+const requestLogger = RequestLogger(/v1\/(interact|collect)\?configId=/, {
+  logRequestBody: true
+});
 
 fixtureFactory({
   title: "C8118: Send information about link clicks.",
   url: linkPageWithClickHandler,
-  requestHooks: [networkLogger.edgeEndpointLogs]
+  requestHooks: [requestLogger]
 });
 
 test.meta({
@@ -22,9 +22,9 @@ test.meta({
 
 test("Test C8118: Load page with link. Click link. Verify request.", async () => {
   await t.click(Selector("#alloy-link-test"));
-  await responseStatus(networkLogger.edgeEndpointLogs.requests, 204);
-  const gatewayRequest = networkLogger.edgeEndpointLogs.requests[0];
+  await t.expect(requestLogger.count(() => true)).gt(0);
+  const gatewayRequest = requestLogger.requests[0];
   const requestBody = JSON.parse(gatewayRequest.request.body);
   const destinationUrl = requestBody.events[0].xdm.web.webInteraction.URL;
-  await t.expect(destinationUrl).contains("missing.html");
+  await t.expect(destinationUrl).contains("blank.html");
 });
