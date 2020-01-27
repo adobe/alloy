c86b8e45b9274868f19eb2f404506bcb91db817f
Nina Ciocanu <nciocanu@adobe.com>
2020-01-27 13:44:54 -0800

    adding one more test for scripts.js

--- test/unit/specs/components/Personalization/helper/scripts.spec.js
+++ test/unit/specs/components/Personalization/helper/scripts.spec.js
@@ -13,7 +13,8 @@ governing permissions and limitations under the License.
 import {
   getInlineScripts,
   getRemoteScriptsUrls,
-  executeInlineScripts
+  executeInlineScripts,
+  executeRemoteScripts
 } from "../../../../../../src/components/Personalization/helper/scripts";
 import cleanUpDomChanges from "../../../../helpers/cleanUpDomChanges";
 import { createFragment } from "../../../../../../src/components/Personalization/helper/dom";
@@ -77,5 +78,19 @@ describe("Personalization::helper::scripts", () => {
     executeInlineScripts(container, inlineScripts, func);
 
     expect(func).toHaveBeenCalledWith(container, inlineScripts[0]);
+  });
+
+  it("should load remote script", () => {
+    const fragmentHTML = "<script src='http://foo.com/' ></script>";
+    const fragment = createFragment(fragmentHTML);
+    const urls = getRemoteScriptsUrls(fragment);
+    const promises = executeRemoteScripts(urls);
+    promises
+      .then(arr => {
+        expect(arr[0].src).toEqual("http://foo.com/");
+      })
+      .catch(() => {
+        fail("should not fail");
+      });
   });
 });
