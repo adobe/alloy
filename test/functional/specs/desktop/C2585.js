e314339453c8c239c31dae08d8d462cc6cd93972
Joe Khoury <jfkhoury@gmail.com>
2020-02-14 12:40:46 -0500

    Update test C2585

--- test/functional/specs/desktop/C2585.js
+++ test/functional/specs/desktop/C2585.js
@@ -1,13 +1,11 @@
-import { Selector } from "testcafe";
+import { ClientFunction } from "testcafe";
 import testServerUrl from "../../helpers/constants/testServerUrl";
 import fixtureFactory from "../../helpers/fixtureFactory";
-
-const urlCollector = `${testServerUrl}/test/functional/sandbox/html/alloySdk.html`;
 
 fixtureFactory({
   title:
     "C2585: Throws error when configure is not the first command executed.",
-  url: urlCollector
+  url: `${testServerUrl}/alloyTestPage.html`
 });
 
 test.meta({
@@ -16,15 +14,26 @@ test.meta({
   TEST_RUN: "Regression"
 });
 
+const triggerAlloyEvent = ClientFunction(() => {
+  return new Promise(resolve => {
+    window
+      .alloy("configure", {
+        errorsEnabled: false,
+        debugEnabled: true
+      })
+      .catch(() => {
+        window.alloy("event", { xdm: { key: "value" } }).catch(resolve);
+      });
+  });
+});
+
 test("Test C2585: Throw error when configure is not the first command executed.", async t => {
   // Note: unable to enable logging with url parameter or enabler logger config.
 
-  await t.click(Selector("#logger-button"));
-  await t.click(Selector("#event-button"));
-  const { log } = await t.getBrowserConsoleMessages();
+  const errorMessage = await triggerAlloyEvent();
 
   await t
-    .expect(log)
+    .expect(errorMessage)
     .match(
       /The library must be configured first. Please do so by executing the configure command./
     );
