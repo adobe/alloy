3f08724a315b3aedbb673bb8372d35232ddd71cd
Gary Rogers <garogers@adobe.com>
2020-03-09 15:29:35 -0600

    Implement C14407, Consenting to all purposes should be persisted

---
+++ test/functional/helpers/cookies.js
@@ -0,0 +1,35 @@
+import { ClientFunction } from "testcafe";
+
+export default {
+  clear: ClientFunction(() => {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i += 1) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+    }
+  }),
+  get: ClientFunction(name => {
+    const cookies = document.cookie
+      .split(";")
+      .map(function(c) {
+        const ct = c.trim();
+        const index = ct.indexOf("=");
+        return [ct.slice(0, index), ct.slice(index + 1)].map(
+          decodeURIComponent
+        );
+      })
+      .reduce(function(a, b) {
+        try {
+          a[b[0]] = JSON.parse(b[1]);
+        } catch (e) {
+          a[b[0]] = b[1];
+        }
+        return a;
+      }, {});
+
+    return cookies[name] || undefined;
+  })
+};
---
+++ test/functional/specs/C14407.js
@@ -0,0 +1,88 @@
+import { t, ClientFunction } from "testcafe";
+import createNetworkLogger from "../helpers/networkLogger";
+import getResponseBody from "../helpers/networkLogger/getResponseBody";
+import fixtureFactory from "../helpers/fixtureFactory";
+import cookies from "../helpers/cookies";
+import alloyEvent from "../helpers/alloyEvent";
+
+const networkLogger = createNetworkLogger();
+
+fixtureFactory({
+  title: "C14407 - Consenting to all purposes should be persisted.",
+  requestHooks: [networkLogger.edgeEndpointLogs]
+});
+
+test.meta({
+  ID: "C14407",
+  SEVERITY: "P0",
+  TEST_RUN: "Regression"
+});
+
+const setConsentIn = ClientFunction(() => {
+  return window.alloy("setConsent", { general: "in" });
+});
+
+test("C14407 - Consenting to all purposes should be persisted.", async () => {
+  const imsOrgId = "53A16ACB5CC1D3760A495C99@AdobeOrg";
+  await cookies.clear();
+  // await apiCalls(imsOrgId);
+
+  const configure = await alloyEvent("configure", {
+    configId: "9999999",
+    orgId: imsOrgId,
+    debugEnabled: true,
+    idMigrationEnabled: false
+  });
+
+  await configure.promise;
+
+  // send alloy event
+  const event1 = await alloyEvent({
+    viewStart: true
+  });
+
+  // apply user consent
+  await setConsentIn();
+
+  await event1.promise;
+
+  const cookieName = `kndctr_${imsOrgId.replace(
+    /[@]+?/,
+    "_"
+  )}_consent`.toString();
+
+  const consentCheck = await cookies.get(cookieName);
+
+  await t.expect(consentCheck).eql("general=in");
+
+  // test konductor response
+  await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(1);
+  await t
+    .expect(networkLogger.edgeEndpointLogs.requests[0].response.statusCode)
+    .eql(200);
+
+  const body = getResponseBody(networkLogger.edgeEndpointLogs.requests[0]);
+  const bodyObject = JSON.parse(body);
+
+  // read state:store handles from response (i.e. 'set a cookie')
+  const cookiesToSet = {};
+
+  await t.expect("handle" in bodyObject).ok();
+  await t.expect(bodyObject.handle.length).gt(0);
+
+  await bodyObject.handle.map(handle => {
+    if (handle.type !== "state:store") return handle;
+    handle.payload.map(payload => {
+      cookiesToSet[payload.key] = payload;
+      return payload;
+    });
+    return handle;
+  });
+
+  // expect that konductor cookie handle matches cookie name
+  await t.expect(cookieName in cookiesToSet).ok();
+
+  // expect that the cookie max age is greater than 0
+  await t.expect("maxAge" in cookiesToSet[cookieName]).ok();
+  await t.expect(cookiesToSet[cookieName].maxAge).gt(0);
+});
