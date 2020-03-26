6615ec308e98be16a1cf3d4473a1449ced0cd242
Joe Khoury <jfkhoury@gmail.com>
2020-03-26 07:24:17 -0400

    Run github workflow on every PR.

--- test/functional/helpers/fixtureFactory/index.js
+++ test/functional/helpers/fixtureFactory/index.js
@@ -4,6 +4,8 @@ const edgeEnv = process.env.EDGE_ENV || "";
 const edgeBasePath = `window.edgeBasePath = ${edgeEnv};`;
 const defaultUrl = `${testServerUrl}/alloyTestPage.html`;
 
+console.log(process.env.GIT_ENV);
+
 export default ({ title = "", url = defaultUrl, requestHooks = [] }) => {
   return fixture(title)
     .page(url)
