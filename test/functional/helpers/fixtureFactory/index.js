4957f86c6704737c83b7baedd21689b1e1d2d59a
Joe Khoury <jfkhoury@gmail.com>
2020-03-27 09:10:10 -0400

    Change the EDGE_ENV from path to a hint.

--- test/functional/helpers/fixtureFactory/index.js
+++ test/functional/helpers/fixtureFactory/index.js
@@ -1,12 +1,10 @@
 import testServerUrl from "../constants/testServerUrl";
 
-const edgeEnv = process.env.EDGE_ENV || "";
-const edgeBasePath = `window.edgeBasePath = ${edgeEnv};`;
+// TODO: Switch to a different page for PROD testing.
 const defaultUrl = `${testServerUrl}/alloyTestPage.html`;
 
 export default ({ title = "", url = defaultUrl, requestHooks = [] }) => {
   return fixture(title)
     .page(url)
-    .clientScripts({ content: edgeBasePath })
     .requestHooks(...requestHooks);
 };
