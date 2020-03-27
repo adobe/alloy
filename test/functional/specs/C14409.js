18d5cf740517bd9d29552da90c9fc746ec8af922
alshaw <alshaw@adobe.com>
2020-03-27 11:45:23 -0600

    Removing response payload check and cookie check

--- test/functional/specs/C14409.js
+++ test/functional/specs/C14409.js
@@ -1,11 +1,8 @@
 import { t, ClientFunction } from "testcafe";
 import createNetworkLogger from "../helpers/networkLogger";
-import getResponseBody from "../helpers/networkLogger/getResponseBody";
 import fixtureFactory from "../helpers/fixtureFactory";
 import cookies from "../helpers/cookies";
 import alloyEvent from "../helpers/alloyEvent";
-import getConsentCookieName from "../helpers/getConsentCookieName";
-import createResponse from "../../../src/core/createResponse";
 
 const networkLogger = createNetworkLogger();
 
@@ -28,6 +25,7 @@ test("C14409 - Consenting to no purposes should be persisted.", async () => {
   const imsOrgId = "53A16ACB5CC1D3760A495C99@AdobeOrg";
   await cookies.clear();
 
+  // configure alloy
   const configure = await alloyEvent("configure", {
     configId: "9999999",
     orgId: imsOrgId,
@@ -37,45 +35,10 @@ test("C14409 - Consenting to no purposes should be persisted.", async () => {
 
   await configure.promise;
 
-  // send alloy event
-  const event1 = await alloyEvent({
-    viewStart: true
-  });
-
-  // apply user consent
+  // set consent to out
   await setConsentToOut();
 
-  await event1.promise;
-
-  const cookieName = getConsentCookieName(imsOrgId);
-
-  const consentCheck = await cookies.get(cookieName);
-
-  await t.expect(consentCheck).eql("general=out");
-
-  // test konductor response
-  await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(1);
-  await t
-    .expect(networkLogger.edgeEndpointLogs.requests[0].response.statusCode)
-    .eql(200);
-
-  const request = JSON.parse(
-    getResponseBody(networkLogger.edgeEndpointLogs.requests[0])
-  );
-
-  // read state:store handles from response (i.e. 'set a cookie')
-  await t.expect("handle" in request).ok();
-  await t.expect(request.handle.length).gt(0);
-
-  const storePayloads = createResponse(request).getPayloadsByType(
-    "state:store"
-  );
-  const cookiesToSet = storePayloads.reduce((memo, storePayload) => {
-    memo[storePayload.key] = storePayload;
-    return memo;
-  }, {});
-
-  // Reload and reconfigure alloy
+  // Reload page and reconfigure alloy
   // [TODO] Navigate to a different subdomain when it is available
   // https://github.com/DevExpress/testcafe/blob/a4f6a4ac3627ebeb29b344ed3a1793627dd87909/docs/articles/documentation/test-api/actions/navigate.md
   await t.eval(() => document.location.reload());
@@ -89,18 +52,14 @@ test("C14409 - Consenting to no purposes should be persisted.", async () => {
 
   await reconfigure.promise;
 
+  // send event
   const errorMessage = await t.eval(() =>
     window
       .alloy("event", { data: { a: 1 } })
       .then(() => undefined, e => e.message)
   );
 
-  // expect that konductor cookie handle matches cookie name
-  await t.expect(cookieName in cookiesToSet).ok();
-
+  // check that the promise from the command is rejected and check the error message to make sure it was due to the user being opted out.
   await t.expect(errorMessage).ok("Expected the event command to be rejected");
   await t.expect(errorMessage).contains("The user declined consent.");
-
-  await t.expect("maxAge" in cookiesToSet[cookieName]).ok();
-  await t.expect(cookiesToSet[cookieName].maxAge).eql(15552000);
 });
