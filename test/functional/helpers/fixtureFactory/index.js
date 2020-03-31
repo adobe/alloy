b27486960b46e71f8a1b807f3b515fc57cd8f2a0
Joe Khoury <jfkhoury@gmail.com>
2020-03-31 14:32:49 -0400

    Inject scripts from fixture.
    The script injection from the testcafe config file was tripping the cron jon for some strange reason, and we had to repeat the scripts for pages with querystrings.

--- test/functional/helpers/fixtureFactory/index.js
+++ test/functional/helpers/fixtureFactory/index.js
@@ -1,10 +1,34 @@
 import testServerUrl from "../constants/testServerUrl";
 import createNetworkLogger from "../networkLogger";
 
+const env = process.env.EDGE_ENV || "int";
+
+const path = require("path");
+
+const pageSnippetPath = path.join(__dirname, "..", "alloyPageSnippet/index.js");
+const alloyLibraryPath = path.join(
+  __dirname,
+  "../../../../",
+  "dist/standalone/alloy.js"
+);
+
 const networkLogger = createNetworkLogger();
 
 export default ({ title = "", url = testServerUrl, requestHooks = [] }) => {
-  return fixture(title)
+  const fixtureObject = fixture(title)
     .page(url)
     .requestHooks(...requestHooks.concat(networkLogger.demdexProxy));
+
+  if (env === "int") {
+    fixtureObject.clientScripts([
+      {
+        path: pageSnippetPath
+      },
+      {
+        path: alloyLibraryPath
+      }
+    ]);
+  }
+
+  return fixtureObject;
 };
