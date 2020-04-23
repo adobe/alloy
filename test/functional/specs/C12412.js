94b1623a8d6c611d98c8b2a5ebca6234dcdeaa6a
Shamiul Mowla <sham.mowla@gmail.com>
2020-04-23 12:26:42 -0400

    Alloy is receiving what it expects, removed additional assertion.

--- test/functional/specs/C12412.js
+++ test/functional/specs/C12412.js
@@ -3,8 +3,6 @@ import createNetworkLogger from "../helpers/networkLogger";
 import fixtureFactory from "../helpers/fixtureFactory";
 import { orgMainConfigMain } from "../helpers/constants/configParts";
 import configureAlloyInstance from "../helpers/configureAlloyInstance";
-import createResponse from "../../../src/core/createResponse";
-import getResponseBody from "../helpers/networkLogger/getResponseBody";
 
 const networkLogger = createNetworkLogger();
 fixtureFactory({
@@ -27,16 +25,6 @@ const getDocumentCookie = ClientFunction(() => document.cookie);
 test("C12412 Response should return Cookie destinations if turned on in Blackbird", async () => {
   await configureAlloyInstance("alloy", orgMainConfigMain);
   await triggerAlloyEvent();
-  const response = JSON.parse(
-    getResponseBody(networkLogger.edgeEndpointLogs.requests[0])
-  );
-  const alloyResponse = createResponse(response);
-  const audiencesPayload = alloyResponse.getPayloadsByType("activation:push");
-  // This is a destination set for all visitors.
-  const destinationForAllVisitors = audiencesPayload.find(
-    payload => payload.type === "cookie" && payload.spec.value === "test=C12412"
-  );
-  await t.expect(destinationForAllVisitors).ok();
 
   await t.expect(getDocumentCookie()).contains("C12412=test=C12412");
 });
