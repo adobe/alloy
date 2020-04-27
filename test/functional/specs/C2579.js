3b669bdb1e8443dc898340656f565b0b555e5b4f
Joe Khoury <jfkhoury@gmail.com>
2020-04-27 16:00:14 -0400

    Return the Alloy command directly.

--- test/functional/specs/C2579.js
+++ test/functional/specs/C2579.js
@@ -62,21 +62,15 @@ const getIdentityCookieValue = request => {
 
 const instance1Config = () => configureAlloyInstance(altConfig);
 
-const instance1Event = ClientFunction(() => {
-  return new Promise(resolve => {
-    return window.alloy("sendEvent", { data: { key: "value" } }).then(resolve);
-  });
-});
+const instance1Event = ClientFunction(() =>
+  window.alloy("sendEvent", { data: { key: "value" } })
+);
 
 const instance2Config = () => configureAlloyInstance("instance2", mainConfig);
 
-const instance2Event = ClientFunction(() => {
-  return new Promise(resolve => {
-    return window
-      .instance2("sendEvent", { data: { key: "value" } })
-      .then(resolve);
-  });
-});
+const instance2Event = ClientFunction(() =>
+  window.instance2("sendEvent", { data: { key: "value" } })
+);
 
 test("Test C2579: Separate ECIDs are used for multiple SDK instances.", async () => {
   await instance1Config();
