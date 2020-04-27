bbfcb1f35c734c07aee988fdfa7ab3164becd134
Shamiul Mowla <sham.mowla@gmail.com>
2020-04-27 11:41:54 -0400

    Alloy is receiving what it expects, removed additional assertion.

--- test/functional/specs/C31436.js
+++ test/functional/specs/C31436.js
@@ -3,8 +3,6 @@ import createNetworkLogger from "../helpers/networkLogger";
 import fixtureFactory from "../helpers/fixtureFactory";
 import { orgMainConfigMain } from "../helpers/constants/configParts";
 import configureAlloyInstance from "../helpers/configureAlloyInstance";
-import createResponse from "../../../src/core/createResponse";
-import getResponseBody from "../helpers/networkLogger/getResponseBody";
 
 const networkLogger = createNetworkLogger();
 
@@ -38,18 +36,6 @@ const triggerAlloyEvent = ClientFunction(() => {
 test("C31436 Qualify for URL destinations via XDM Data.", async () => {
   await configureAlloyInstance("alloy", orgMainConfigMain);
   await triggerAlloyEvent();
-  const response = JSON.parse(
-    getResponseBody(networkLogger.edgeEndpointLogs.requests[0])
-  );
-  const alloyResponse = createResponse(response);
-  const audiencesPayload = alloyResponse.getPayloadsByType("activation:push");
-  // This is a destination set for all visitors.
-  const destinationForAllVisitors = audiencesPayload.find(
-    payload =>
-      payload.type === "url" &&
-      payload.spec.url === "https://cataas.com/cat/cute"
-  );
-  await t.expect(destinationForAllVisitors).ok();
 
   await t.expect(destinationLogger.requests.length > 0).eql(true);
 });
