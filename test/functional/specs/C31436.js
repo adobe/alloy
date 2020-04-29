9b7e5c506e01dab5d42cb372d48d9104b23e0387
Shamiul Mowla <sham.mowla@gmail.com>
2020-04-29 11:47:16 -0400

    C31436 - Qualify for URL destinations via Context Data (#417)
    * C31436 Qualify for URL destinations via XDM Data.
    
    * Alloy is receiving what it expects, removed additional assertion.
    
    * Assert 1 request went to the destination

---
+++ test/functional/specs/C31436.js
@@ -0,0 +1,41 @@
+import { RequestLogger, t, ClientFunction } from "testcafe";
+import createNetworkLogger from "../helpers/networkLogger";
+import fixtureFactory from "../helpers/fixtureFactory";
+import { orgMainConfigMain } from "../helpers/constants/configParts";
+import configureAlloyInstance from "../helpers/configureAlloyInstance";
+
+const networkLogger = createNetworkLogger();
+
+const networkLoggerConfig = {
+  logRequestBody: true,
+  stringifyRequestBody: true
+};
+
+const destinationLogger = RequestLogger(
+  "https://cataas.com/cat/cute",
+  networkLoggerConfig
+);
+
+fixtureFactory({
+  title: "C31436 Qualify for URL destinations via XDM Data.",
+  requestHooks: [networkLogger.edgeEndpointLogs, destinationLogger]
+});
+
+test.meta({
+  ID: "C31436",
+  SEVERITY: "P0",
+  TEST_RUN: "Regression"
+});
+
+const triggerAlloyEvent = ClientFunction(() => {
+  return window.alloy("event", {
+    xdm: { web: { webPageDetails: { name: "C31436" } } }
+  });
+});
+
+test("C31436 Qualify for URL destinations via XDM Data.", async () => {
+  await configureAlloyInstance("alloy", orgMainConfigMain);
+  await triggerAlloyEvent();
+
+  await t.expect(destinationLogger.requests.length).eql(1);
+});
