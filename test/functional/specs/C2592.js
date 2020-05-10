0d75add23928875247af1e16a4622c681ecf70ee
Joe Khoury <jfkhoury@gmail.com>
2020-05-10 13:37:44 -0400

    Update functional tests.

--- test/functional/specs/C2592.js
+++ test/functional/specs/C2592.js
@@ -27,8 +27,9 @@ test.meta({
 });
 
 const triggerAlloyEvent = ClientFunction(() => {
-  return new Promise(resolve => {
-    window.alloy("sendEvent", { xdm: { key: "value" } }).then(() => resolve());
+  return window.alloy("sendEvent", {
+    xdm: { key: "value" },
+    datasetId: "5eb83a3f634d1618a8a1d3df"
   });
 });
 
@@ -45,6 +46,9 @@ test("Test C2592: Event command sends a request.", async () => {
   );
 
   await t.expect(request.events[0].xdm.key).eql("value");
+  await t
+    .expect(request.events[0].meta.collect.datasetId)
+    .eql("5eb83a3f634d1618a8a1d3df");
   await t.expect(request.meta.state.cookiesEnabled).eql(true);
   await t.expect(request.meta.state.domain).ok();
 });
