b9d686db49af5e56e0b5496f1e1453bb0f8dae1d
Jon Snyder <josnyder@adobe.com>
2020-06-04 10:49:08 -0600

    Update setConsent Validation

--- src/components/Privacy/validateSetConsentOptions.js
+++ src/components/Privacy/validateSetConsentOptions.js
@@ -1,20 +1,38 @@
-import { enumOf, objectOf, literal, arrayOf } from "../../utils/validation";
+import {
+  enumOf,
+  objectOf,
+  literal,
+  arrayOf,
+  anyOf,
+  string,
+  boolean
+} from "../../utils/validation";
 import { IN, OUT } from "../../constants/consentStatus";
 import { GENERAL } from "../../constants/consentPurpose";
 
 export default objectOf({
   consent: arrayOf(
-    objectOf({
-      standard: literal("Adobe").required(),
-      version: literal("1.0").required(),
-      value: objectOf({
-        [GENERAL]: enumOf(IN, OUT).required()
+    anyOf([
+      objectOf({
+        standard: literal("Adobe").required(),
+        version: literal("1.0").required(),
+        value: objectOf({
+          [GENERAL]: enumOf(IN, OUT).required()
+        })
+          .noUnknownFields()
+          .required()
+      })
+        .noUnknownFields()
+        .required(),
+      objectOf({
+        standard: literal("IAB").required(),
+        version: literal("2.0").required(),
+        value: string().required(),
+        gdprApplies: boolean().required()
       })
         .noUnknownFields()
         .required()
-    })
-      .noUnknownFields()
-      .required()
+    ])
   )
     .nonEmpty()
     .required()
