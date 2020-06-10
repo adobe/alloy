3a024bbe716a00de075cdc43b26968be61381240
Jon Snyder <josnyder@adobe.com>
2020-06-10 10:51:13 -0600

    Fix setConsent validation integration tests

--- src/components/Privacy/validateSetConsentOptions.js
+++ src/components/Privacy/validateSetConsentOptions.js
@@ -12,27 +12,30 @@ import { GENERAL } from "../../constants/consentPurpose";
 
 export default objectOf({
   consent: arrayOf(
-    anyOf([
-      objectOf({
-        standard: literal("Adobe").required(),
-        version: literal("1.0").required(),
-        value: objectOf({
-          [GENERAL]: enumOf(IN, OUT).required()
+    anyOf(
+      [
+        objectOf({
+          standard: literal("Adobe").required(),
+          version: literal("1.0").required(),
+          value: objectOf({
+            [GENERAL]: enumOf(IN, OUT).required()
+          })
+            .noUnknownFields()
+            .required()
+        })
+          .noUnknownFields()
+          .required(),
+        objectOf({
+          standard: literal("IAB").required(),
+          version: literal("2.0").required(),
+          value: string().required(),
+          gdprApplies: boolean().required()
         })
           .noUnknownFields()
           .required()
-      })
-        .noUnknownFields()
-        .required(),
-      objectOf({
-        standard: literal("IAB").required(),
-        version: literal("2.0").required(),
-        value: string().required(),
-        gdprApplies: boolean().required()
-      })
-        .noUnknownFields()
-        .required()
-    ])
+      ],
+      "a valid consent object"
+    )
   )
     .nonEmpty()
     .required()
