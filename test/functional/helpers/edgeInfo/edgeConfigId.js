6a9bdf34c7a6d9d85c3b913c107a234cc17b21d8
Jon Snyder <josnyder@adobe.com>
2020-07-07 09:22:11 -0600

    Update edgeConfigId for INT. The old one was deleted from Blackbird

--- test/functional/helpers/edgeInfo/edgeConfigId.js
+++ test/functional/helpers/edgeInfo/edgeConfigId.js
@@ -1,7 +1,7 @@
 const env = process.env.EDGE_ENV || "int";
 
 const configIds = {
-  int: "f70f48c8-ca4b-4f4b-a4dc-087382148892",
+  int: "5c5f7e16-1ed2-41ce-b817-3fa7adb705b5",
   prod: "60928f59-0406-4353-bfe3-22ed633c4f67"
 };
 
