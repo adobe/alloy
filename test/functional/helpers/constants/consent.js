512d45da74eff60db2399cf48d566fd3946513a1
Joe Khoury <jfkhoury@gmail.com>
2020-07-16 16:38:08 -0400

    C224670 Opt in to IAB.

--- test/functional/helpers/constants/consent.js
+++ test/functional/helpers/constants/consent.js
@@ -20,3 +20,48 @@ export const CONSENT_OUT = {
     }
   ]
 };
+
+// TODO: Add a test case with no `gdprApplies`
+export const IAB_CONSENT_IN = {
+  consent: [
+    {
+      standard: "IAB TCF",
+      version: "2.0",
+      value: "CO052l-O052l-DGAMBFRACBgAIBAAAAAAIYgEawAQEagAAAA",
+      gdprApplies: true
+    }
+  ]
+};
+
+export const IAB_NO_PURPOSE_ONE = {
+  consent: [
+    {
+      standard: "IAB TCF",
+      version: "2.0",
+      value: "CO052oTO052oTDGAMBFRACBgAABAAAAAAIYgEawAQEagAAAA",
+      gdprApplies: true
+    }
+  ]
+};
+
+export const IAB_NO_PURPOSE_TEN = {
+  consent: [
+    {
+      standard: "IAB TCF",
+      version: "2.0",
+      value: "CO052kIO052kIDGAMBFRACBgAIAAAAAAAIYgEawAQEagAAAA",
+      gdprApplies: true
+    }
+  ]
+};
+
+export const IAB_NO_ADOBE_VENDOR = {
+  consent: [
+    {
+      standard: "IAB TCF",
+      version: "2.0",
+      value: "CO052qdO052qdDGAMBFRACBgAIBAAAAAAIYgAAoAAAAA",
+      gdprApplies: true
+    }
+  ]
+};
---
+++ test/functional/specs/Privacy/IAB/C224670.js
@@ -0,0 +1,85 @@
+import { t, ClientFunction } from "testcafe";
+import createNetworkLogger from "../../../helpers/networkLogger";
+import { responseStatus } from "../../../helpers/assertions/index";
+import createFixture from "../../../helpers/createFixture";
+import configureAlloyInstance from "../../../helpers/configureAlloyInstance";
+import createResponse from "../../../../../src/core/createResponse";
+import getResponseBody from "../../../helpers/networkLogger/getResponseBody";
+import cookies from "../../../helpers/cookies";
+import {
+  compose,
+  orgMainConfigMain,
+  consentPending,
+  debugEnabled
+} from "../../../helpers/constants/configParts";
+
+const config = compose(
+  orgMainConfigMain,
+  consentPending,
+  debugEnabled
+);
+
+const { IAB_CONSENT_IN } = require("../../../helpers/constants/consent");
+
+const networkLogger = createNetworkLogger();
+
+createFixture({
+  title: "C224670: Opt in to IAB using the setConsent command.",
+  requestHooks: [
+    networkLogger.setConsentEndpointLogs,
+    networkLogger.edgeEndpointLogs
+  ]
+});
+
+test.meta({
+  ID: "C224670",
+  SEVERITY: "P0",
+  TEST_RUN: "REGRESSION"
+});
+
+const triggerSetConsent = ClientFunction(
+  () => window.alloy("setConsent", IAB_CONSENT_IN),
+  { dependencies: { IAB_CONSENT_IN } }
+);
+
+const sendEvent = ClientFunction(() => window.alloy("sendEvent"));
+
+test("Test C224670: Opt in to IAB", async () => {
+  await configureAlloyInstance("alloy", config);
+  await triggerSetConsent();
+
+  await t.expect(networkLogger.setConsentEndpointLogs.requests.length).eql(1);
+  await responseStatus(networkLogger.edgeEndpointLogs.requests, 200);
+
+  const consentRawResponse = JSON.parse(
+    getResponseBody(networkLogger.setConsentEndpointLogs.requests[0])
+  );
+
+  const consentResponse = createResponse(consentRawResponse);
+
+  // 1. The set-consent response should contain the Consent cookie: { general: in }
+  const consentCookieName = "kndctr_334F60F35E1597910A495EC2_AdobeOrg_consent";
+  const consentCookieValue = await cookies.get(consentCookieName);
+
+  await t.expect(consentCookieValue).eql("general=in");
+  await t.expect(consentCookieValue).ok("No consent cookie found.");
+
+  // 2. The set-consent response payload contains the consent handle in XDM format
+  const consentHandle = consentResponse.getPayloadsByType("privacy:consent");
+
+  await t.expect(consentHandle.length).gte(0);
+  await t.expect(consentHandle[0]).eql({
+    consentStandard: "IAB TCF",
+    consentStandardVersion: "2.0",
+    consentStringValue: "CO052l-O052l-DGAMBFRACBgAIBAAAAAAIYgEawAQEagAAAA",
+    containsPersonalData: false,
+    gdprApplies: true
+  });
+
+  // 3. The ECID should exist in the response payload as well, if queried
+  const identityHandle = consentResponse.getPayloadsByType("identity:result");
+  await t.expect(identityHandle.length).eql(1);
+
+  await sendEvent();
+  await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(1);
+});
