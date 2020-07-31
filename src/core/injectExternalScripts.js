c48cd98827d1700dee08dc3b2603a5298dd36cc6
Don Cottingham <cottingh@adobe.com>
2020-07-31 08:39:46 -0700

    add ability to load external scripts before alloy is initialized on page

---
+++ src/core/injectExternalScripts.js
@@ -0,0 +1,53 @@
+import { corsRequest } from "./network/injectSendXhrRequest";
+
+const createResultLogMessage = (scriptUrl, success) => {
+  return `Loading script ${success ? "succeeded" : "failed"}: ${scriptUrl}`;
+};
+
+// TODO - change this to cdn hosted target.js url after done testing
+const SOLUTION_SCRIPTS = {
+  mboxEdit: `http://localhost:4000/target.js/?${new Date().getTime()}`
+};
+
+const getExternalScripts = () => {
+  let params = document.location.href.split("?");
+  if (params.length === 1) {
+    return [];
+  }
+  params = params[1].split("&");
+  return params
+    .map(param => {
+      const paramName = param.split("=")[0];
+      return SOLUTION_SCRIPTS[paramName];
+    })
+    .filter(Boolean);
+};
+
+const createScriptRequest = scriptUrl => {
+  const options = {
+    method: "GET",
+    url: scriptUrl
+  };
+  return corsRequest(window.XMLHttpRequest, options)
+    .then(response => {
+      const scriptjs = response.body;
+      // eslint-disable-next-line no-eval
+      window.eval(scriptjs);
+      console.log(createResultLogMessage(scriptUrl, true));
+    })
+    .catch(() => {
+      console.error(createResultLogMessage(scriptUrl, false));
+    });
+};
+
+export default () => {
+  const scriptUrls = getExternalScripts();
+
+  if (!scriptUrls.length) {
+    return Promise.resolve();
+  }
+
+  return Promise.all(
+    scriptUrls.map(scriptUrl => createScriptRequest(scriptUrl))
+  );
+};
--- src/core/network/injectSendXhrRequest.js
+++ src/core/network/injectSendXhrRequest.js
@@ -10,31 +10,53 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
+export const corsRequest = (XMLHttpRequest, options) => {
+  const { method, url, body, responseType, withCredentials } = options;
+  const headers = options.headers || {};
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if (request.status === 0) {
+          reject(new Error("Request aborted."));
+        } else {
+          resolve({
+            status: request.status,
+            body: request.responseText
+          });
+        }
+      }
+    };
+    request.onloadstart = () => {
+      request.responseType = responseType;
+    };
+    request.open(method, url, true);
+    Object.keys(headers).map(header =>
+      request.setRequestHeader(header, headers[header])
+    );
+    request.withCredentials = withCredentials;
+    request.onerror = reject;
+    request.onabort = reject;
+    if (body) {
+      request.send(body);
+    } else {
+      request.send();
+    }
+  });
+};
+
 export default XMLHttpRequest => {
   return (url, body) => {
-    return new Promise((resolve, reject) => {
-      const request = new XMLHttpRequest();
-      request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-          if (request.status === 0) {
-            reject(new Error("Request aborted."));
-          } else {
-            resolve({
-              status: request.status,
-              body: request.responseText
-            });
-          }
-        }
-      };
-      request.onloadstart = () => {
-        request.responseType = "text";
-      };
-      request.open("POST", url, true);
-      request.setRequestHeader("Content-Type", "text/plain; charset=UTF-8");
-      request.withCredentials = true;
-      request.onerror = reject;
-      request.onabort = reject;
-      request.send(body);
-    });
+    const options = {
+      method: "POST",
+      responseType: "text",
+      withCredentials: true,
+      headers: {
+        "Content-Type": "text/plain; charset=UTF-8"
+      },
+      url,
+      body
+    };
+    return corsRequest(XMLHttpRequest, options);
   };
 };
