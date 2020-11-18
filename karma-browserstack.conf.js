a2aa6592fd01ed206de05765d0d91d108789c46d
Shamiul Mowla <sham.mowla@gmail.com>
2020-11-18 14:06:14 -0500

    Add the list of browsers previously used with the unit tests.

--- karma-browserstack.conf.js
+++ karma-browserstack.conf.js
@@ -12,6 +12,27 @@ module.exports = config => {
       browser_version: "72.0",
       os: "Windows",
       os_version: "10"
+    },
+    bs_safari_macos: {
+      base: "BrowserStack",
+      browser: "Safari",
+      browser_version: "13.1",
+      os: "OS X",
+      os_version: "Catalina"
+    },
+    bs_firefox_windows: {
+      base: "BrowserStack",
+      browser: "Firefox",
+      browser_version: "latest-beta",
+      os: "Windows",
+      os_version: "10"
+    },
+    bs_ie_windows: {
+      base: "BrowserStack",
+      browser: "IE",
+      browser_version: "11.0",
+      os: "Windows",
+      os_version: "10"
     }
   };
 
