90caae2bcb66c6082f68189b9635a9ab726a8af4
Jon Snyder <josnyder@adobe.com>
2021-02-24 14:49:08 -0700

    Make sure the set consent requests are sent sequentially

--- test/functional/specs/Privacy/C14414.js
+++ test/functional/specs/Privacy/C14414.js
@@ -8,6 +8,7 @@ import {
 import { CONSENT_IN, CONSENT_OUT } from "../../helpers/constants/consent";
 import createAlloyProxy from "../../helpers/createAlloyProxy";
 import createNetworkLogger from "../../helpers/networkLogger";
+import SequentialHook from "../../helpers/requestHooks/sequentialHook";
 
 const config = compose(
   orgMainConfigMain,
@@ -28,17 +29,32 @@ test.meta({
   TEST_RUN: "Regression"
 });
 
-test("Test C14414: Requests are queued while consent changes are pending", async t => {
-  const alloy = createAlloyProxy();
-  await alloy.configure(config);
-  const setConsentResponse1 = await alloy.setConsentAsync(CONSENT_IN);
-  const setConsentResponse2 = await alloy.setConsentAsync(CONSENT_OUT);
-  await alloy.sendEvent();
+test
+  .before(async t => {
+    t.ctx.setConsentHook = new SequentialHook(/v1\/privacy\/set-consent\?/);
+    await t.addRequestHooks(t.ctx.setConsentHook);
+  })
+  .after(async t => {
+    await t.removeRequestHooks(t.ctx.setConsentHook);
+  })(
+  "Test C14414: Requests are queued while consent changes are pending",
+  async t => {
+    const alloy = createAlloyProxy();
+    await alloy.configure(config);
+    const setConsentResponse1 = await alloy.setConsentAsync(CONSENT_IN);
+    const setConsentResponse2 = await alloy.setConsentAsync(CONSENT_OUT);
+    await alloy.sendEvent();
 
-  // make sure there are no errors returned from the setConsent requests
-  await setConsentResponse1.result;
-  await setConsentResponse2.result;
+    // make sure there are no errors returned from the setConsent requests
+    await setConsentResponse1.result;
+    await setConsentResponse2.result;
 
-  // make sure the event was not sent
-  await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(0);
-});
+    // make sure the event was not sent
+    await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(0);
+
+    await t
+      .expect(t.ctx.setConsentHook.haveRequestsBeenSequential())
+      .ok("Set-consent requests were not sequential");
+    await t.expect(t.ctx.setConsentHook.getNumRequests()).eql(2);
+  }
+);
