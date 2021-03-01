7a2a0fe577d335deb995c63133c59810d639a5dd
Nina Ciocanu <nciocanu@adobe.com>
2021-03-01 19:34:53 +0200

    some refactoring

--- src/components/Personalization/createExecuteCachedViewDecisions.js
+++ src/components/Personalization/createExecuteCachedViewDecisions.js
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { isNonEmptyArray } from "../../utils";
+import isNonEmptyArray from "../../utils/isNonEmptyArray";
 
 export default ({ viewCache, executeViewDecisions, collect }) => {
   return ({ viewName }) => {
