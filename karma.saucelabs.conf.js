4b33b628e40e4fe16b60a62a72deb1e2943f4dce
Shamiul Mowla <sham.mowla@gmail.com>
2021-03-19 15:55:43 -0400

    Add supported browsers to unit test.

--- karma.saucelabs.conf.js
+++ karma.saucelabs.conf.js
@@ -4,7 +4,6 @@ const karmaConfig = require("./karma.conf.js");
 
 module.exports = config => {
   karmaConfig(config);
-
   const customLaunchers = {
     sl_chromeW3C: {
       base: 'SauceLabs',
@@ -26,16 +25,13 @@ module.exports = config => {
       base: 'SauceLabs',
       browserName: 'safari',
       browserVersion: 'latest',
-      'sauce:options':{
-        tags: ['w3c-safari']
-      }
+      platform: 'macOS 11.00',
     },
     sl_ieW3C: {
       base: 'SauceLabs',
-      browserName: 'ie',
+      browserName: 'internet explorer',
       browserVersion: 'latest',
-      'sauce:options':{
-        tags: ['w3c-ie']
+      platform: 'Windows 10',
       }
     },
   };
