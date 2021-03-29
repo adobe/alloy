43ac1d6c9252d46d7d81f6053063083765bb38d3
Gary Rogers <garogers@adobe.com>
2021-03-29 13:20:08 -0600

    PDCL-1352 - Adds test lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,17 @@ module.exports = {
     node: true,
     jasmine: true
   },
-  plugins: ["prettier", "testcafe"],
+  plugins: ["ban", "prettier", "testcafe"],
   rules: {
+    "ban/ban": [
+      "error",
+      { name: ["describe", "only"], message: "don't focus tests" },
+      { name: "fdescribe", message: "don't focus tests" },
+      { name: ["it", "only"], message: "don't focus tests" },
+      { name: "fit", message: "don't focus tests" },
+      { name: ["test", "only"], message: "don't focus tests" },
+      { name: "ftest", message: "don't focus tests" }
+    ],
     "prettier/prettier": "error",
     "no-param-reassign": ["error", { props: false }],
     "func-style": "error",
