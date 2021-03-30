aa8dcd8c76c6ace4cf7da16db7d2b14dca649248
Nina Ciocanu <nciocanu@adobe.com>
2021-03-30 17:32:21 +0300

    fixing and improving the functional test

--- test/functional/specs/Personalization/C205528.js
+++ test/functional/specs/Personalization/C205528.js
@@ -1,24 +1,19 @@
-import { t, ClientFunction, RequestLogger } from "testcafe";
+import { t, RequestLogger } from "testcafe";
 import createNetworkLogger from "../../helpers/networkLogger";
 import createFixture from "../../helpers/createFixture";
-import configureAlloyInstance from "../../helpers/configureAlloyInstance";
-import {
-  compose,
-  orgMainConfigMain,
-  debugEnabled
-} from "../../helpers/constants/configParts";
+import { orgMainConfigMain } from "../../helpers/constants/configParts";
 import { TEST_PAGE as TEST_PAGE_URL } from "../../helpers/constants/url";
+import { responseStatus } from "../../helpers/assertions";
+import createAlloyProxy from "../../helpers/createAlloyProxy";
+import getResponseBody from "../../helpers/networkLogger/getResponseBody";
+import createResponse from "../../helpers/createResponse";
 
 const networkLogger = createNetworkLogger();
 
 const redirectLogger = RequestLogger(
-  "https://alloyio.com/functional-test/alloyTestPage.html?redirectedTest=true"
+  "https://alloyio.com/functional-test/alloyTestPage.html?redirectedTest=true&test=C205528"
 );
 
-const config = compose(
-  orgMainConfigMain,
-  debugEnabled
-);
 createFixture({
   title:
     "C205528 A redirect offer should redirect the page to the URL in the redirect decision",
@@ -31,20 +26,44 @@ test.meta({
   SEVERITY: "P0",
   TEST_RUN: "Regression"
 });
+const assertRedirectNotificationType = async notificationEvents => {
+  await t.expect(notificationEvents[0].xdm.eventType).eql("redirect");
+};
 
-const triggerAlloyEvent = ClientFunction(() => {
-  return window.alloy("sendEvent", {
-    renderDecisions: true
-  });
-});
+const assertScopeDetails = async (decision, notificationEvents) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const meta = notificationEvents[0].xdm._experience.decisioning.propositions;
+  await t.expect(meta[0].scopeDetails).eql(decision.scopeDetails);
+};
+
+const getDecisions = () => {
+  const response = JSON.parse(
+    getResponseBody(networkLogger.edgeEndpointLogs.requests[0])
+  );
+  return createResponse({
+    content: response
+  }).getPayloadsByType("personalization:decisions");
+};
 
 test("Test C205528: A redirect offer should redirect the page to the URL in the redirect decision", async () => {
-  await configureAlloyInstance("alloy", config);
-  try {
-    await triggerAlloyEvent();
-  } catch (e) {
-    // we'll get the ClientFunction exception here because within it we will do a redirect.
-  } finally {
-    await t.expect(redirectLogger.requests.length).eql(1);
-  }
+  const alloy = createAlloyProxy();
+  await alloy.configure(orgMainConfigMain);
+
+  await alloy.sendEvent({ renderDecisions: true });
+  await responseStatus(networkLogger.edgeEndpointLogs.requests, 200);
+
+  const decisions = getDecisions(networkLogger);
+  const redirectNotification = networkLogger.edgeEndpointLogs.requests[1];
+
+  const redirectNotificationBody = JSON.parse(
+    redirectNotification.request.body
+  );
+  const notificationEvents = redirectNotificationBody.events;
+
+  // expect the redirect notification is sent
+  await assertRedirectNotificationType(notificationEvents);
+  await assertScopeDetails(decisions[0], notificationEvents);
+
+  // expect the redirect was executed
+  await t.expect(redirectLogger.requests.length).eql(1);
 });
