29ace285e173f91f4dc67a233a98c7af1519f67b
Nina Ceban Ciocanu <ninaceban@users.noreply.github.com>
2021-05-25 14:53:18 -0700

    PDCL-1245 - Support redirect Personalization offer types (#656)
    Co-authored-by: Nina Ciocanu <nciocanu@adobe.com>

---
+++ src/components/Personalization/createRedirectHandler.js
@@ -0,0 +1,35 @@
+/*
+Copyright 2021 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+const getRedirectDetails = redirectDecisions => {
+  const decision = redirectDecisions[0];
+  const { items, id, scope } = decision;
+  const { content } = items[0].data;
+
+  return { content, decisions: [{ id, scope }] };
+};
+
+export default ({ collect, window, logger, showContainers }) => {
+  return redirectDecisions => {
+    const { content, decisions } = getRedirectDetails(redirectDecisions);
+    const documentMayUnload = true;
+
+    return collect({ decisionsMeta: decisions, documentMayUnload })
+      .then(() => {
+        window.location.replace(content);
+      })
+      .catch(() => {
+        showContainers();
+        logger.warn("An error occurred while executing the redirect offer.");
+      });
+  };
+};
---
+++ test/functional/specs/Personalization/C205528.js
@@ -0,0 +1,46 @@
+import { t, RequestLogger } from "testcafe";
+import createNetworkLogger from "../../helpers/networkLogger";
+import createFixture from "../../helpers/createFixture";
+import {
+  compose,
+  orgMainConfigMain,
+  debugEnabled
+} from "../../helpers/constants/configParts";
+import { TEST_PAGE as TEST_PAGE_URL } from "../../helpers/constants/url";
+import createAlloyProxy from "../../helpers/createAlloyProxy";
+
+const networkLogger = createNetworkLogger();
+const redirectEndpoint = /functional-test\/alloyTestPage.html\?redirectedTest=true/;
+
+const redirectLogger = RequestLogger(redirectEndpoint);
+
+const config = compose(
+  orgMainConfigMain,
+  debugEnabled
+);
+createFixture({
+  title:
+    "C205528 A redirect offer should redirect the page to the URL in the redirect decision",
+  url: `${TEST_PAGE_URL}?test=C205528`,
+  requestHooks: [networkLogger.edgeEndpointLogs, redirectLogger]
+});
+
+test.meta({
+  ID: "C205528",
+  SEVERITY: "P0",
+  TEST_RUN: "Regression"
+});
+
+test("Test C205528: A redirect offer should redirect the page to the URL in the redirect decision", async () => {
+  const alloy = createAlloyProxy();
+  await alloy.configure(config);
+  try {
+    await alloy.sendEvent({
+      renderDecisions: true
+    });
+  } catch (e) {
+    // an exception will be thrown because a redirect will be executed within the Alloy Client Function
+  } finally {
+    await t.expect(redirectLogger.requests.length).eql(1);
+  }
+});
