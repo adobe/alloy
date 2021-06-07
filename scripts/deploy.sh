1244bfbcac464a8ea0ec722ca1e9b31ec00de0bc
Jon Snyder <josnyder@adobe.com>
2021-06-07 13:20:39 -0600

    Verify files are available on the CDN

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -40,3 +40,22 @@ npm run build
 echo "$CDN_PRIVATE_KEY" > id_rsa
 ./scripts/sftpCommands.sh ${VERSION} | \
   sftp -i ./id_rsa -oHostKeyAlgorithms=+ssh-dss -b - sshacs@dxresources.ssh.upload.akamai.com:/prod/alloy
+
+# verify the files are available on the cdn
+ALLOY_MIN_JS="https://cdn1.adoberesources.net/alloy/${VERSION}/alloy.min.js"
+STATUS_CODE=$(curl -o /dev/null -Isw '%{http_code}\n' $ALLOY_MIN_JS)
+if [ $STATUS_CODE != "200" ]; then
+  echo "curl request to $ALLOY_MIN_JS returned status code $STATUS_CODE" 1>&2
+  exit 1
+fi
+
+ALLOY_JS="https://cdn1.adoberesources.net/alloy/${VERSION}/alloy.js"
+STATUS_CODE=$(curl -o /dev/null -Isw '%{http_code}\n' "${ALLOY_JS}")
+if [ $STATUS_CODE != "200" ]; then
+  echo "curl request to $ALLOY_JS returned status code $STATUS_CODE" 1>&2
+  exit 1
+fi
+
+echo "Verified files are available on CDN"
+
+
