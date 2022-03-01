1f4b314324644789f75a0dd5550940b58a35169d
Carter McBride <cmcbride@adobe.com>
2022-03-01 08:15:59 -0700

    Tidy up functional test

--- test/functional/specs/Personalization/C5298194.js
+++ test/functional/specs/Personalization/C5298194.js
@@ -15,7 +15,7 @@ test.meta({
   TEST_RUN: "Regression"
 });
 
-test("Test C5298194: Include propositions on every request", async () => {
+const runTest = async () => {
   const collectEndpointAsserter = await createCollectEndpointAsserter();
   const alloy = createAlloyProxy();
   await alloy.configure(orgMainConfigMain);
@@ -43,37 +43,11 @@ test("Test C5298194: Include propositions on every request", async () => {
     .ok("response.propositions is not an array");
   response = null;
   await collectEndpointAsserter.reset();
-});
+};
+
+test("Test C5298194: Include propositions on every request", runTest);
 
 test.page(`${TEST_PAGE_URL}?adobe_authoring_enabled=true`)(
   "Test C5298194: Include propositions on every request in authoring mode",
-  async () => {
-    const collectEndpointAsserter = await createCollectEndpointAsserter();
-    const alloy = createAlloyProxy();
-    await alloy.configure(orgMainConfigMain);
-
-    // check that interact calls return propositions.
-    let response = await alloy.sendEvent();
-    await collectEndpointAsserter.assertInteractCalledAndNotCollect();
-    await t
-      .expect("propositions" in response)
-      .ok("The 'sendEvent()' response was missing the 'propositions' property");
-    await t
-      .expect(Array.isArray(response.propositions))
-      .ok("response.propositions is not an array");
-    response = null;
-    await collectEndpointAsserter.reset();
-
-    // check that collect calls return propositions.
-    response = await alloy.sendEvent({ documentUnloading: true });
-    await collectEndpointAsserter.assertCollectCalledAndNotInteract();
-    await t
-      .expect("propositions" in response)
-      .ok("The 'sendEvent()' response was missing the 'propositions' property");
-    await t
-      .expect(Array.isArray(response.propositions))
-      .ok("response.propositions is not an array");
-    response = null;
-    await collectEndpointAsserter.reset();
-  }
+  runTest
 );
