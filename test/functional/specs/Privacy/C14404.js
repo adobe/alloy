6121e523ff661cc84446ebdb10b843824f799d28
Anca Suliman <asuliman@adobe.com>
2022-03-14 19:07:53 +0200

    [PDCL-7369] - Tests updates to support opt-back-in functionality

--- test/functional/specs/Privacy/C14404.js
+++ test/functional/specs/Privacy/C14404.js
@@ -1,3 +1,4 @@
+import { t } from "testcafe";
 import createFixture from "../../helpers/createFixture";
 import createNetworkLogger from "../../helpers/networkLogger";
 
@@ -7,9 +8,10 @@ import {
   consentPending,
   debugEnabled
 } from "../../helpers/constants/configParts";
-import createConsoleLogger from "../../helpers/consoleLogger";
 import createAlloyProxy from "../../helpers/createAlloyProxy";
 import { CONSENT_OUT, CONSENT_IN } from "../../helpers/constants/consent";
+import cookies from "../../helpers/cookies";
+import { MAIN_CONSENT_COOKIE_NAME } from "../../helpers/constants/cookies";
 
 const config = compose(orgMainConfigMain, consentPending, debugEnabled);
 
@@ -27,21 +29,22 @@ test.meta({
   TEST_RUN: "Regression"
 });
 
-test("Test C14404: User cannot consent to all purposes after consenting to no purposes", async t => {
+test("Test C14404: User can consent to all purposes after consenting to no purposes", async () => {
   const alloy = createAlloyProxy();
   await alloy.configure(config);
   await alloy.setConsent(CONSENT_OUT);
-  const setConsentErrorMessage = await alloy.setConsentErrorMessage(CONSENT_IN);
 
-  await t
-    .expect(setConsentErrorMessage)
-    .ok("Expected the setConsent command to be rejected");
-  await t.expect(setConsentErrorMessage).contains("EXEG-0302-409");
+  // set consent back to in for the same user
+  await alloy.setConsent(CONSENT_IN);
 
-  // make sure the instance still has no consent
-  const logger = await createConsoleLogger();
+  await t.expect(networkLogger.setConsentEndpointLogs.requests.length).eql(2);
+
+  const consentCookieValue = await cookies.get(MAIN_CONSENT_COOKIE_NAME);
+
+  await t.expect(consentCookieValue).ok("No consent cookie found.");
+  await t.expect(consentCookieValue).eql("general=in");
+
+  // make sure event goes out
   await alloy.sendEvent();
-  await logger.warn.expectMessageMatching(/user declined consent/);
-  // make sure no event requests went out
-  await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(0);
+  await t.expect(networkLogger.edgeEndpointLogs.requests.length).eql(1);
 });
