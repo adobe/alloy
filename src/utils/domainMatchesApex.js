f8dfaa9074807881f3ca365b11af5970b5f29f05
Carter McBride <cmcbride@adobe.com>
2022-03-17 15:54:58 -0600

    Create function to see if a domain fits inside the apex domain

---
+++ src/utils/domainMatchesApex.js
@@ -0,0 +1,19 @@
+/*
+Copyright 2022 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+/**
+ * Determines if a domain fits within the provided apex domain.
+ * @param {!string} domain A domain name, e.g. subdomain.example.com
+ * @param {string} apexDomain An apex domain, e.g. example.com
+ * @returns {!boolean}
+ */
+export default (domain, apexDomain) => domain.includes(apexDomain);
