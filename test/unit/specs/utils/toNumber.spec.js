ce623b8cfc0e4da0c6f8f39cb071ca7c4281e25e
Jon Snyder <josnyder@adobe.com>
2022-05-19 14:57:14 -0600

    Fixed an issue where localTimezoneOffset could fail Adobe Experience Platform validation.

--- test/unit/specs/utils/toNumber.spec.js
+++ test/unit/specs/utils/toNumber.spec.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Adobe. All rights reserved.
+Copyright 2022 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -9,21 +9,26 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-import { deepAssign, toISOStringLocal } from "../../utils";
-import toNumber from "../../utils/toNumber";
 
-export default dateProvider => {
-  return xdm => {
-    const date = dateProvider();
+import toNumber from "../../../../src/utils/toNumber";
 
-    const placeContext = {
-      localTime: toISOStringLocal(date)
-    };
+describe("toNumber", () => {
+  [
+    ["0", 0],
+    ["1", 1],
+    ["true", undefined],
+    ["1.1", 1.1],
+    ["-4", -4],
+    ["123abc", undefined],
+    [-42, -42],
+    [3.14, 3.14]
+  ].forEach(([input, output]) => {
+    it(`converts "${input}" to ${output}`, () => {
+      expect(toNumber(input)).toEqual(output);
+    });
+  });
 
-    const localTimezoneOffset = toNumber(date.getTimezoneOffset());
-    if (localTimezoneOffset !== undefined) {
-      placeContext.localTimezoneOffset = localTimezoneOffset;
-    }
-    deepAssign(xdm, { placeContext });
-  };
-};
+  it("uses the passed value for the default", () => {
+    expect(toNumber("foo", 0)).toEqual(0);
+  });
+});
