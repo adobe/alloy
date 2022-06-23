566c9f0c355b15143c7b7e0ed7e986bd35d1f21f
Jon Snyder <josnyder@adobe.com>
2022-06-23 14:50:58 -0600

    Move test into Location Hints folder, remove trailing slash from regex

--- test/functional/specs/Location Hints/C6589015.js
+++ test/functional/specs/Location Hints/C6589015.js
@@ -34,14 +34,15 @@ test("Test C6589015: The Experience Edge location hint is used on the second req
   const alloy = createAlloyProxy();
   await alloy.configure(config);
   await alloy.sendEvent({});
-  await alloy.sendEvent({});
 
   const locationHint = await cookies.get(MAIN_CLUSTER_COOKIE_NAME);
   await t.expect(locationHint).ok();
+
+  await alloy.sendEvent({});
 
   const urls = networkLogger.edgeEndpointLogs.requests.map(r => r.request.url);
   await t.expect(urls[0]).match(new RegExp("^https://[^/]+/[^/]+/v1/interact"));
   await t
     .expect(urls[1])
-    .match(new RegExp(`^https://[^/]+/[^/]+/${locationHint}/v1/interact/`));
+    .match(new RegExp(`^https://[^/]+/[^/]+/${locationHint}/v1/interact`));
 });
