b328b96f37292d0a4b5e3eaf024974313c4a4245
Jon Snyder <josnyder@adobe.com>
2022-08-04 16:24:58 -0600

    Use ci instead of install

--- samples/install.sh
+++ samples/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-(cd ../ && npm install && npm run sandbox:build)
-npm install
-(cd personalization-client-side && npm install && npm run build)
-(cd personalization-hybrid && npm install)
-(cd personalization-hybrid-spa && npm install && npm run build)
-(cd personalization-server-side && npm install)
-(cd sandbox && npm install && npm run build)
+(cd ../ && npm ci && npm run sandbox:build)
+npm ci
+(cd personalization-client-side && npm ci && npm run build)
+(cd personalization-hybrid && npm ci)
+(cd personalization-hybrid-spa && npm ci && npm run build)
+(cd personalization-server-side && npm ci)
+(cd sandbox && npm ci && npm run build)
 
