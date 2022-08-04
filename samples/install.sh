4ab23fb3da588e16c04d7f3fa0ddfb9f8cc9a88f
Jon Snyder <josnyder@adobe.com>
2022-08-04 16:16:00 -0600

    npm install in the samples directory as part of install

--- samples/install.sh
+++ samples/install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 (cd ../ && npm install && npm run sandbox:build)
+npm install
 (cd personalization-client-side && npm install && npm run build)
 (cd personalization-hybrid && npm install)
 (cd personalization-hybrid-spa && npm install && npm run build)
