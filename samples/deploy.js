c6e3a9ae8932c1b83c916d3b72a82149cc380880
Jon Snyder <josnyder@adobe.com>
2022-08-04 14:20:40 -0600

    Use GITHUB_REF_NAME to get the branch name

--- samples/deploy.js
+++ samples/deploy.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 const pm2 = require("pm2-deploy");
 
-const ref = process.env.GITHUB_REF;
+const branch = process.env.GITHUB_REF_NAME;
 
-if (!ref || !ref.startsWith("refs/heads")) {
-  console.error(`GITHUB_REF is a required env var and must start with refs/heads, got ${ref}`);
+if (!branch) {
+  console.error(`GITHUB_REF_NAME is a required env var, got ${ref}`);
   process.exit(1);
 }
-const branch = ref.substring("refs/heads".length);
 const path = `/var/www/${branch}.alloyio.com`;
+console.log(`Deploying to ${path}`);
 
 const deployConfig = {
   "deploy": {
     "prod": {
       "user": "jonsnyder",
       "host": ["localhost"],
-      ref,
+      ref: `refs/heads/${branch}`,
       "repo": "git@github.com:adobe/alloy.git",
       path,
       "post-deploy": "./install.sh"
