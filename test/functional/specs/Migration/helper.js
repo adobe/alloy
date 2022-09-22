eade05d62ae26687a6689f1f3bedbb70d9a81c97
Nina Ciocanu <nciocanu@adobe.com>
2022-09-23 01:28:26 +0300

    refactoring

---
+++ test/functional/specs/Migration/helper.js
@@ -0,0 +1,76 @@
+import { ClientFunction, t } from "testcafe";
+import readCache from "read-cache";
+import createAlloyProxy from "../../helpers/createAlloyProxy";
+import createConsoleLogger from "../../helpers/consoleLogger";
+import { injectAlloyDuringTest } from "../../helpers/createFixture/clientScripts";
+import cookies from "../../helpers/cookies";
+
+export const AT_JS_VERSION_TWO =
+  "./sandbox/public/functional-test/legacyJs/at2.js";
+export const AT_JS_VERSION_ONE =
+  "./sandbox/public/functional-test/legacyJs/at.js";
+
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const getAtjsVersion = ClientFunction(() => {
+  const target = window.adobe.target;
+
+  return target.VERSION;
+});
+
+export const extractCluster = hostname => {
+  const values = hostname.split(".");
+  return values[0];
+};
+
+export const injectAlloyAndSendEvent = async config => {
+  const alloy = createAlloyProxy();
+  await alloy.configureAsync(config);
+  await alloy.getLibraryInfoAsync();
+  const logger = await createConsoleLogger();
+  await injectAlloyDuringTest();
+  await logger.info.expectMessageMatching(/Executing getLibraryInfo command/);
+  await alloy.sendEvent();
+};
+
+export const assertTargetMigrationEnabledIsSent = async requestBody => {
+  await t.expect(requestBody.meta.target).eql({ migration: true });
+};
+
+export const assertKonductorReturnsCookieAndCookieIsSet = async (
+  cookieKey,
+  stateStorePayload
+) => {
+  const responseContainsCookie = stateStorePayload.find(entry => {
+    return entry.key.includes(cookieKey);
+  });
+  await t.expect(responseContainsCookie).ok();
+  // Check that cookie is set
+  const cookieValue = await cookies.get(cookieKey);
+  await t.expect(cookieValue).ok();
+
+  return cookieValue;
+};
+
+const getLocalAtjsCode = libraryPath => {
+  // readCache caches file content until the file is modified, at which
+  // point it will retrieve fresh file content, cache it, and return it.
+  return readCache.sync(libraryPath, "utf8");
+};
+
+const injectAtjsScript = ClientFunction(code => {
+  const scriptElement = document.createElement("script");
+  // eslint-disable-next-line no-undef
+  scriptElement.innerHTML = code;
+  document.getElementsByTagName("head")[0].appendChild(scriptElement);
+});
+
+export const injectAtjsOnThePage = async (libraryPath, libraryVersion) => {
+  const localAtjs = getLocalAtjsCode(libraryPath);
+  await injectAtjsScript(localAtjs);
+
+  const version = await getAtjsVersion();
+  await t.expect(version).eql(libraryVersion);
+  // we need this to make sure at.js had enough time to trigger the delivery request
+  await sleep(1000);
+};
