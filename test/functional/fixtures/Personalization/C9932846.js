4921f6874c1c320fd11cfe58a19ab3cc7516e624
Andrei Anischevici <anischev@adobe.com>
2023-03-02 23:36:15 +0200

    Add AJO click-tracking functional test

---
+++ test/functional/fixtures/Personalization/C9932846.js
@@ -0,0 +1,14 @@
+export const testPageBody = `
+<div id="root">
+  <div>
+    <ul>
+      <li><a href="/">Home</a></li>
+    </ul>
+    <hr>
+    <div>
+      <h1>AJO Personalization</h1>
+    </div>
+    </div>
+  </div>
+</div>
+`;
