9ac96ed24ebfd5ee6ba807cc58468f2ccd145c40
Happy Shandilya <happyshandilya@gmail.com>
2023-05-12 10:55:40 -0700

    following convention

--- src/utils/parseUrl.js
+++ src/utils/parseUrl.js
@@ -9,8 +9,8 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
+import parseUri from "parse-uri";
 import isString from "./isString";
-import parseUri from "./parseUri";
 
 const parseDomainBasic = host => {
   const result = {};
