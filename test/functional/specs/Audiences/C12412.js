407ec5227788f8e7f218c38de51dcf5a8ed5d987
Shamiul Mowla <sham.mowla@gmail.com>
2023-05-18 21:23:38 -0400

    Local saucelabs functional test cross browser support (#993)
    * Standalone browser specific Saucelabs runner scripts to debug locally cross environment.
    
    * Clear cache for edge ee workflow.
    
    * Added parse-uri and js-cookie test run dependencies.

--- test/functional/specs/Audiences/C12412.js
+++ test/functional/specs/Audiences/C12412.js
@@ -21,22 +21,25 @@ import createAlloyProxy from "../../helpers/createAlloyProxy";
 import createConsoleLogger from "../../helpers/consoleLogger";
 
 const networkLogger = createNetworkLogger();
-createFixture({
-  title:
-    "C12412 Response should return Cookie destinations if turned on in Blackbird",
-  requestHooks: [networkLogger.edgeEndpointLogs]
-});
-test.meta({
-  ID: "C12412",
-  SEVERITY: "P0",
-  TEST_RUN: "Regression"
-});
+
+createFixture(
+  `C12412 Response should return Cookie destinations if turned on in Blackbird`,
+  {
+    requestHooks: [networkLogger.edgeEndpointLogs],
+    meta: {
+      ID: "C12412",
+      SEVERITY: "P0",
+      TEST_RUN: "Regression"
+    }
+  }
+);
 
 const getDocumentCookie = ClientFunction(() => document.cookie);
 
-test("C12412 Response should return Cookie destinations if turned on in Blackbird", async () => {
+test(`Verify cookie destinations are returned in the response when turned on in Blackbird`, async () => {
   const logger = await createConsoleLogger();
   const alloy = createAlloyProxy();
+
   await alloy.configure(compose(orgMainConfigMain, debugEnabled));
   await alloy.sendEvent();
 
@@ -44,9 +47,10 @@ test("C12412 Response should return Cookie destinations if turned on in Blackbir
 
   const logs = await logger.info.getMessagesSinceReset();
   const setCookieAttributes = logs
-    .filter(message => message.length === 3 && message[1] === "Setting cookie")
+    .filter(message => message.length === 3 && message[1] === `Setting cookie`)
     .map(message => message[2])
     .filter(cookieSettings => cookieSettings.name === "C12412");
+
   await t.expect(setCookieAttributes.length).eql(1);
   await t.expect(setCookieAttributes[0].sameSite).eql("none");
   await t.expect(setCookieAttributes[0].secure).eql(true);
