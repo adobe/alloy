92fd6091c8525aeef7ae36d57558e67eb18d38b4
Carter McBride <18412686+carterworks@users.noreply.github.com>
2023-07-11 15:00:11 -0600

    Fix loop in Propositions SPA sandbox page (#1010)
    * Fix (most) eslint errors in sandbox
    
    * Value-compare data and xdm in useEffect dependency arrays
    
    Every time the useSendPageViewEvent() function was being called without
    the xdm and data params being specified, the default parameter would
    create a new object. This would make the function run again aka it was
    not safe to call multiple times because the data and xdm variables
    didn't pass the === test.
    Since the values are small, there isn't much worry about performance.
    See this comment about the subject: https://github.com/facebook/react/issues/14476#issuecomment-471199055

--- sandbox/src/DataInsertionAPI.js
+++ sandbox/src/DataInsertionAPI.js
@@ -1,14 +1,16 @@
 const trackingServer = "ujsl.sc.omtrdc.net";
 const reportSuite = "ujslecommerce";
 
-export function sendAnalyticsPayload({ analyticsPayload, visitorID }) {
+export const sendAnalyticsPayload = ({ analyticsPayload, visitorID }) => {
   const url = `https://${trackingServer}/b/ss/${reportSuite}/0?g=${window.location}&r=${document.referrer}&mid=${visitorID}&tnta=${analyticsPayload}`;
 
   return fetch(url)
     .then(success => {
+      // eslint-disable-next-line no-console
       console.log("success", success);
     })
     .catch(error => {
+      // eslint-disable-next-line no-console
       console.warn("error while triggering Analytics hit", error);
     });
-}
+};
