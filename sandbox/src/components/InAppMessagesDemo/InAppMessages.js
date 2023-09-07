d6fdfcab2bb846b07eeb58bd7303bbd48c99ef61
Jason Waters <jasonwaters@users.noreply.github.com>
2023-09-07 13:32:18 -0600

    support "Send data to platform" trigger (#1030)

--- sandbox/src/components/InAppMessagesDemo/InAppMessages.js
+++ sandbox/src/components/InAppMessagesDemo/InAppMessages.js
@@ -1,12 +1,51 @@
+/* eslint-disable no-bitwise */
 import React, { useState, useEffect } from "react";
 import ContentSecurityPolicy from "../ContentSecurityPolicy";
 import "./InAppMessagesStyle.css";
 
+const configKey = "cjmProdNld2";
+
+const config = {
+  cjmProdNld2: {
+    datastreamId: "7a19c434-6648-48d3-948f-ba0258505d98",
+    surface: "mobileapp://com.adobe.iamTutorialiOS",
+    decisionContext: {
+      "~type": "com.adobe.eventType.generic.track",
+      "~source": "com.adobe.eventSource.requestContent",
+      state: "",
+      "~state.com.adobe.module.lifecycle/lifecyclecontextdata.dayofweek": 1
+    },
+    activeCampaigns: [
+      "https://experience.adobe.com/#/@cjmprodnld2/sname:prod/journey-optimizer/campaigns/summary/59bfdc09-03b9-4cd5-9ab8-5c2a045b0b2e"
+    ]
+  },
+  aemonacpprodcampaign: {
+    datastreamId: "8cefc5ca-1c2a-479f-88f2-3d42cc302514",
+    surface: "mobileapp://com.adobe.aguaAppIos",
+    decisionContext: {},
+    activeCampaigns: [
+      "https://experience.adobe.com/#/@aemonacpprodcampaign/sname:prod/journey-optimizer/campaigns/summary/8bb52c05-d381-4d8b-a67a-95f345776322"
+    ]
+  }
+};
+
+const { datastreamId, surface, decisionContext } = config[configKey];
+
+const uuidv4 = () => {
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+};
+
 export default function DecisionEngine() {
   const [realResponse, setRealResponse] = useState(null);
+
   const getInAppPayload = async payload => {
     const res = await fetch(
-      `https://edge.adobedc.net/ee/or2/v1/interact?configId=7a19c434-6648-48d3-948f-ba0258505d98&requestId=520353b2-dc0d-428c-9e0d-138fc6cbec4e`,
+      `https://edge.adobedc.net/ee/or2/v1/interact?configId=${datastreamId}&requestId=${uuidv4()}`,
       {
         method: "POST",
         body: JSON.stringify(payload)
@@ -22,7 +61,7 @@ export default function DecisionEngine() {
           {
             query: {
               personalization: {
-                surfaces: ["mobileapp://com.adobe.iamTutorialiOS"]
+                surfaces: [surface]
               }
             },
             xdm: {
@@ -44,21 +83,10 @@ export default function DecisionEngine() {
   const renderDecisions = e => {
     e.stopPropagation();
     e.preventDefault();
-    window.alloy("evaluateRulesets", {
-      "~type": "com.adobe.eventType.generic.track",
-      "~source": "com.adobe.eventSource.requestContent",
-      state: "",
-      "~state.com.adobe.module.lifecycle/lifecyclecontextdata.dayofweek": 1
-    });
 
     window.alloy("applyResponse", {
       renderDecisions: true,
-      decisionContext: {
-        "~type": "com.adobe.eventType.generic.track",
-        "~source": "com.adobe.eventSource.requestContent",
-        state: "",
-        "~state.com.adobe.module.lifecycle/lifecyclecontextdata.dayofweek": 1
-      },
+      decisionContext,
       responseBody: realResponse
     });
   };
