f8e588399491df7cc3097ddf69ff09b04a47458b
Happy Shandilya <happyshandilya@gmail.com>
2023-10-30 16:04:49 -0700

    back button bug fix (#1077)
    * navigate back to the previous page using the browser's history.
    
    * preserveHistory
    
    * keeping comment

--- src/components/Personalization/dom-actions/createRedirect.js
+++ src/components/Personalization/dom-actions/createRedirect.js
@@ -10,8 +10,12 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-export default window => url => {
-  window.location.replace(url);
+export default window => (url, preserveHistory = false) => {
+  if (preserveHistory) {
+    window.location.href = url;
+  } else {
+    window.location.replace(url);
+  }
   // Return a promise that never resolves because redirects never complete
   // within the current page.
   return new Promise(() => undefined);
