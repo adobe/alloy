277901ac18cb8cee215a0803b031e38890749921
Shamiul Mowla <sham.mowla@gmail.com>
2023-11-17 12:17:56 -0500

    componentNames update.

--- scripts/helpers/componentNames.js
+++ scripts/helpers/componentNames.js
@@ -1,13 +1,41 @@
-module.exports = {
-  datacollector: "DataCollector",
-  decisioningengine: "DecisioningEngine",
-  activitycollector: "Activity Collector",
-  identity: "Identity",
-  audiences: "Audiences",
-  personalization: "Personalization",
-  context: "Context",
-  privacy: "Privacy",
-  eventmerge: "Event Merge",
-  libraryinfo: "Library Info",
-  machinelearning: "Machine Learning"
-};
+module.exports = [
+  {
+    name: "datacollector",
+    exportName: "createDataCollector",
+    filePath: "DataCollector"
+  },
+  {
+    name: "decisioningengine",
+    exportName: "createDecisioningEngine",
+    filePath: "DecisioningEngine"
+  },
+  {
+    name: "activitycollector",
+    exportName: "createActivityCollector",
+    filePath: "ActivityCollector"
+  },
+  { name: "identity", exportName: "createIdentity", filePath: "Identity" },
+  { name: "audiences", exportName: "createAudiences", filePath: "Audiences" },
+  {
+    name: "personalization",
+    exportName: "createPersonalization",
+    filePath: "Personalization"
+  },
+  { name: "context", exportName: "createContext", filePath: "Context" },
+  { name: "privacy", exportName: "createPrivacy", filePath: "Privacy" },
+  {
+    name: "eventmerge",
+    exportName: "createEventMerge",
+    filePath: "EventMerge"
+  },
+  {
+    name: "libraryinfo",
+    exportName: "createLibraryInfo",
+    filePath: "LibraryInfo"
+  },
+  {
+    name: "machinelearning",
+    exportName: "createMachineLearning",
+    filePath: "MachineLearning"
+  }
+];
