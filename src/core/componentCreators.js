46119e4b6db735922410378ac1652ba155be7488
Shamiul Mowla <sham.mowla@gmail.com>
2023-11-20 12:22:22 -0500

    Add skipwhen for componentCreators and component naming object.

--- src/core/componentCreators.js
+++ src/core/componentCreators.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Adobe. All rights reserved.
+Copyright 2023 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -15,17 +15,23 @@ governing permissions and limitations under the License.
 import createDataCollector from "../components/DataCollector";
 import createActivityCollector from "../components/ActivityCollector";
 import createIdentity from "../components/Identity";
+/* @skipwhen ENV.alloy_audiences === false */
 import createAudiences from "../components/Audiences";
+/* @skipwhen ENV.alloy_personalization === false */
 import createPersonalization from "../components/Personalization";
 import createContext from "../components/Context";
 import createPrivacy from "../components/Privacy";
+/* @skipwhen ENV.alloy_eventmerge === false */
 import createEventMerge from "../components/EventMerge";
+/* @skipwhen ENV.alloy_libraryinfo === false */
 import createLibraryInfo from "../components/LibraryInfo";
 import createDecisioningEngine from "../components/DecisioningEngine";
+/* @skipwhen ENV.alloy_machinelearning === false */
 import createMachineLearning from "../components/MachineLearning";
 
 // TODO: Register the Components here statically for now. They might be registered differently.
 // TODO: Figure out how sub-components will be made available/registered
+
 export default [
   createDataCollector,
   createActivityCollector,
