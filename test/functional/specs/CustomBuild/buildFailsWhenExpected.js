3b3fd4e1fc528dfaf7e54a509de9c37099a2fae1
Shamiul Mowla <sham.mowla@gmail.com>
2024-01-24 11:20:58 -0500

    Custom build test specs.

---
+++ test/functional/specs/CustomBuild/buildFailsWhenExpected.js
@@ -0,0 +1,20 @@
+/*
+Copyright 2024 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+import { exec } from "child_process";
+
+test("Check if build fails when expected", async () => {
+  exec("npm run build:custom -- --exclude nonExistingComponent", error => {
+    if (!error) {
+      console.error("Expected error but none was thrown");
+    }
+  });
+});
