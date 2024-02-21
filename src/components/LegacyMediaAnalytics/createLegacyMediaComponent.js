81add6d20b55d97acdac2db5738767fbe892f0ca
Nina Ciocanu <nciocanu@adobe.com>
2024-02-21 15:27:22 +0200

    adding code review changes

---
+++ src/components/LegacyMediaAnalytics/createLegacyMediaComponent.js
@@ -0,0 +1,79 @@
+/*
+Copyright 2024 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { noop, uuid } from "../../utils";
+import {
+  AD_METADATA_KEYS as AdMetadataKeys,
+  AUDIO_METADATA_KEYS as AudioMetadataKeys,
+  EVENT as Event,
+  MEDIA_OBJECT_KEYS as MediaObjectKey,
+  MEDIA_TYPE as MediaType,
+  PLAYER_STATE as PlayerState,
+  STREAM_TYPE as StreamType,
+  VIDEO_METADATA_KEYS as VideoMetadataKeys
+} from "./constants/constants";
+
+export default ({
+  trackMediaEvent,
+  trackMediaSession,
+  onBeforeMediaEvent,
+  logger,
+  createMediaHelper,
+  createGetInstance,
+  config
+}) => {
+  return {
+    lifecycle: {
+      onBeforeEvent({ mediaOptions, onResponse = noop }) {
+        const { legacy, playerId, getPlayerDetails } = mediaOptions;
+
+        if (!legacy) {
+          return;
+        }
+        onResponse(({ response }) => {
+          return onBeforeMediaEvent({ playerId, getPlayerDetails, response });
+        });
+      }
+    },
+    commands: {
+      getMediaAnalyticsTracker: {
+        run: () => {
+          if (!config.mediaCollection) {
+            logger.warn("Media Collection is not configured.");
+          }
+          logger.debug("Media Collection is configured in legacy mode.");
+          const mediaAnalyticsHelper = createMediaHelper({ logger });
+
+          return Promise.resolve({
+            getInstance: () => {
+              return createGetInstance({
+                logger,
+                trackMediaEvent,
+                trackMediaSession,
+                uuid
+              });
+            },
+            Event,
+            MediaType,
+            PlayerState,
+            StreamType,
+            MediaObjectKey,
+            VideoMetadataKeys,
+            AudioMetadataKeys,
+            AdMetadataKeys,
+            ...mediaAnalyticsHelper
+          });
+        }
+      }
+    }
+  };
+};
--- src/components/LegacyMediaAnalytics/index.js
+++ src/components/LegacyMediaAnalytics/index.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Adobe. All rights reserved.
+Copyright 2024 Adobe. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -15,20 +15,10 @@ import createMediaEventManager from "../MediaCollection/createMediaEventManager"
 import createMediaSessionCacheManager from "../MediaCollection/createMediaSessionCacheManager";
 import createTrackMediaEvent from "../MediaCollection/createTrackMediaEvent";
 import createTrackMediaSession from "../MediaCollection/createTrackMediaSession";
-import createMediaHelper from "./createMediaHelper";
-import {
-  AD_METADATA_KEYS as AdMetadataKeys,
-  AUDIO_METADATA_KEYS as AudioMetadataKeys,
-  EVENT as Event,
-  MEDIA_OBJECT_KEYS as MediaObjectKey,
-  MEDIA_TYPE as MediaType,
-  PLAYER_STATE as PlayerState,
-  STREAM_TYPE as StreamType,
-  VIDEO_METADATA_KEYS as VideoMetadataKeys
-} from "./constants/constants";
-import createGetInstance from "./createGetInstance";
-import { noop, uuid } from "../../utils";
 import createOnBeforeMediaEvent from "../MediaCollection/createOnBeforeMediaEvent";
+import createLegacyMediaComponent from "./createLegacyMediaComponent";
+import createMediaHelper from "./createMediaHelper";
+import createGetInstance from "./createGetInstance";
 
 const createLegacyMediaAnalytics = ({
   eventManager,
@@ -65,53 +55,16 @@ const createLegacyMediaAnalytics = ({
     config,
     trackMediaEvent
   });
-  return {
-    lifecycle: {
-      onBeforeEvent({ mediaOptions, onResponse = noop }) {
-        const { legacy, playerId, getPlayerDetails } = mediaOptions;
 
-        if (!legacy) {
-          return;
-        }
-        onResponse(({ response }) => {
-          return onBeforeMediaEvent({ playerId, getPlayerDetails, response });
-        });
-      }
-    },
-    commands: {
-      getMediaAnalyticsTracker: {
-        run: () => {
-          if (!config.mediaCollection) {
-            logger.warn("Media Collection is not configured.");
-          }
-
-          logger.info("Media Analytics Legacy Component was configured");
-
-          const mediaAnalyticsHelper = createMediaHelper({ logger });
-
-          return Promise.resolve({
-            getInstance: () => {
-              return createGetInstance({
-                logger,
-                trackMediaEvent,
-                trackMediaSession,
-                uuid
-              });
-            },
-            Event,
-            MediaType,
-            PlayerState,
-            StreamType,
-            MediaObjectKey,
-            VideoMetadataKeys,
-            AudioMetadataKeys,
-            AdMetadataKeys,
-            ...mediaAnalyticsHelper
-          });
-        }
-      }
-    }
-  };
+  return createLegacyMediaComponent({
+    onBeforeMediaEvent,
+    trackMediaSession,
+    trackMediaEvent,
+    createMediaHelper,
+    createGetInstance,
+    logger,
+    config
+  });
 };
 
 createLegacyMediaAnalytics.namespace = "Legacy Media Analytics";
