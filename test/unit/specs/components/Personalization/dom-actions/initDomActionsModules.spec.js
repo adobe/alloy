8b619b6bea551ec17b90afc6c8b463891fb17da4
Jason Waters <waters@adobe.com>
2024-02-27 17:09:44 -0700

    DOM_ACTION constants usage

--- test/unit/specs/components/Personalization/dom-actions/initDomActionsModules.spec.js
+++ test/unit/specs/components/Personalization/dom-actions/initDomActionsModules.spec.js
@@ -10,28 +10,45 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import initDomActionsModules from "../../../../../../src/components/Personalization/dom-actions/initDomActionsModules";
+import initDomActionsModules, {
+  DOM_ACTION_APPEND_HTML,
+  DOM_ACTION_CLICK,
+  DOM_ACTION_CUSTOM_CODE,
+  DOM_ACTION_INSERT_AFTER,
+  DOM_ACTION_INSERT_BEFORE,
+  DOM_ACTION_MOVE,
+  DOM_ACTION_PREPEND_HTML,
+  DOM_ACTION_REARRANGE,
+  DOM_ACTION_REMOVE,
+  DOM_ACTION_REPLACE_HTML,
+  DOM_ACTION_RESIZE,
+  DOM_ACTION_SET_ATTRIBUTE,
+  DOM_ACTION_SET_HTML,
+  DOM_ACTION_SET_IMAGE_SOURCE,
+  DOM_ACTION_SET_STYLE,
+  DOM_ACTION_SET_TEXT
+} from "../../../../../../src/components/Personalization/dom-actions/initDomActionsModules";
 
 const buildSet = () => {
   const result = new Set();
 
   // This is to make IE 11 happy
-  result.add("setHtml");
-  result.add("customCode");
-  result.add("setText");
-  result.add("setAttribute");
-  result.add("setImageSource");
-  result.add("setStyle");
-  result.add("move");
-  result.add("resize");
-  result.add("rearrange");
-  result.add("remove");
-  result.add("insertAfter");
-  result.add("insertBefore");
-  result.add("replaceHtml");
-  result.add("prependHtml");
-  result.add("appendHtml");
-  result.add("click");
+  result.add(DOM_ACTION_SET_HTML);
+  result.add(DOM_ACTION_CUSTOM_CODE);
+  result.add(DOM_ACTION_SET_TEXT);
+  result.add(DOM_ACTION_SET_ATTRIBUTE);
+  result.add(DOM_ACTION_SET_IMAGE_SOURCE);
+  result.add(DOM_ACTION_SET_STYLE);
+  result.add(DOM_ACTION_MOVE);
+  result.add(DOM_ACTION_RESIZE);
+  result.add(DOM_ACTION_REARRANGE);
+  result.add(DOM_ACTION_REMOVE);
+  result.add(DOM_ACTION_INSERT_AFTER);
+  result.add(DOM_ACTION_INSERT_BEFORE);
+  result.add(DOM_ACTION_REPLACE_HTML);
+  result.add(DOM_ACTION_PREPEND_HTML);
+  result.add(DOM_ACTION_APPEND_HTML);
+  result.add(DOM_ACTION_CLICK);
 
   return result;
 };
