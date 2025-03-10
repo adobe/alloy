4431614fcf10fd3c6a4e75cd9891be4968edc0a5
Serban Stancu <stancu@adobe.com>
2025-03-10 16:41:42 -0600

    Add default value back.

--- src/utils/flattenArray.js
+++ src/utils/flattenArray.js
@@ -10,4 +10,4 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-export default (arr) => (Array.isArray(arr) ? arr.flat(Infinity) : arr);
+export default (arr = []) => (Array.isArray(arr) ? arr.flat(Infinity) : arr);
