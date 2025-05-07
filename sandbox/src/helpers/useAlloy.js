420dbd765195ba2720e8ea88ad28ce36ec69b539
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-05-07 09:02:07 -0600

    Fix sandbox to serve alloy, direct from source.

--- sandbox/src/helpers/useAlloy.js
+++ sandbox/src/helpers/useAlloy.js
@@ -34,6 +34,10 @@ const setup = ({
   });
 
   initializeAlloy(window, instanceNames);
+  const alloyScriptPath = new URL(
+    "../../../src/standalone.js",
+    import.meta.url,
+  );
 
   if (getUrlParameter("includeVisitor") === "true") {
     includeScript(
@@ -44,10 +48,10 @@ const setup = ({
         doesOptInApply: getUrlParameter("legacyOptIn") === "true",
       });
       // Alloy only looks for window.Visitor when it initially loads, so only load Alloy after Visitor loaded.
-      includeScript("/alloy.js");
+      includeScript(alloyScriptPath, { module: true });
     });
   } else {
-    includeScript("/alloy.js");
+    includeScript(alloyScriptPath, { module: true });
   }
 
   if (onAlloySetupCompleted) {
@@ -92,6 +96,7 @@ const defaultConfiguration = {
 };
 
 const configureInstance = (instanceName, configuration = {}) => {
+  // TODO: import alloy directly from the dist/src folder.
   window[instanceName]("configure", {
     ...defaultConfiguration,
     ...configuration,
