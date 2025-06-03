923ad4c4bb7d05200643638a7cb12ecbfae1cb7b
Serban Stancu <stancu@adobe.com>
2025-06-03 15:24:11 -0600

    Add option for configuring how alloy is functioning inside integration tests.

--- test/integration/helpers/alloy/config.js
+++ test/integration/helpers/alloy/config.js
@@ -8,8 +8,13 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
+
 export default {
-  orgId: "5BFE274A5F6980A50A495C08@AdobeOrg",
-  datastreamId: "bc1a10e0-aee4-4e0e-ac5b-cdbb9abbec83",
+  orgId: import.meta.env.VITE_ORG_ID || "5BFE274A5F6980A50A495C08@AdobeOrg",
+  datastreamId:
+    import.meta.env.VITE_DATASTREAM_ID ||
+    "bc1a10e0-aee4-4e0e-ac5b-cdbb9abbec83",
+  edgeDomain: import.meta.env.VITE_EDGE_DOMAIN || "edge.adobedc.net",
+  edgeBasePath: import.meta.env.VITE_EDGE_BASE_PATH || "ee",
   thirdPartyCookiesEnabled: false,
 };
