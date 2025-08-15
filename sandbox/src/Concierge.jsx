23593321ea904fc5d66075513ffbf3189ddf85f6
Nina Ciocanu <nciocanu@adobe.com>
2025-08-14 18:53:55 -0700

    changing custom version

--- sandbox/src/Concierge.jsx
+++ sandbox/src/Concierge.jsx
@@ -1,33 +1,32 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import ContentSecurityPolicy from "./components/ContentSecurityPolicy";
 import useAlloy from "./helpers/useAlloy";
-import {styles} from "./acom-hackathon";
+import { styles } from "./acom-hackathon";
 export default function Concierge() {
   useAlloy();
 
   useEffect(() => {
     window["alloy"]("sendEvent", {
-      conversation: {fetchExperience: true}
-    }).then(result=> {
+      conversation: { fetchConversationalExperience: true },
+    }).then((result) => {
+      console.log("Conversation experience fetched", result);
       window["alloy"]("bootstrapConversationalExperience", {
         selector: "#brand-concierge-mount",
         src: "https://experience-stage.adobe.net/solutions/experience-platform-brand-concierge-web-agent/static-assets/main.js",
-        stylingConfigurations: styles
-      })
+        stylingConfigurations: styles,
+      });
     });
   }, []);
 
   return (
     <div>
-      <ContentSecurityPolicy/>
+      <ContentSecurityPolicy />
       <h1>Concierge Demo Page</h1>
       <p>
-        This page will be used to test the Concierge feature of the Adobe Experience Platform.
+        This page will be used to test the Concierge feature of the Adobe
+        Experience Platform.
       </p>
-      <div id="brand-concierge-mount">
-
-      </div>
+      <div id="brand-concierge-mount"></div>
     </div>
-
   );
 }
