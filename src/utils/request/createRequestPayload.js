b462a28a3a0e9431e5c013677184ae013260b23f
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-08-21 16:12:03 -0600

    Fix the sending of override values on disabled services

--- src/utils/request/createRequestPayload.js
+++ src/utils/request/createRequestPayload.js
@@ -12,17 +12,71 @@ governing permissions and limitations under the License.
 
 import { createMerger, prepareConfigOverridesForEdge } from "../index.js";
 
+/**
+ * createMerger creates a function that does a deep merge. Example:
+ * ```js
+ * payload.mergeConfigOverride({
+ *   com_adobe_analytics: {
+ *     reportSuites: ["reportSuite1"],
+ *   },
+ * });
+ *
+ * payload.mergeConfigOverride({
+ *   com_adobe_analytics: {
+ *     enabled: false,
+ *   },
+ * });
+ *
+ *
+ * // payload.meta.configOverrides is now:
+ * // {
+ * //   com_adobe_analytics: {
+ * //     enabled: false,
+ * //     reportSuites: ["reportSuite1"],
+ * //   },
+ * // }
+ * ```
+ * however, we need the result to be:
+ * ```js
+ * // {
+ * //   com_adobe_analytics: {
+ * //     enabled: false,
+ * //   },
+ * // }
+ * ```
+ * aka a shallow merge, where the second object overwrites the first.
+ * This is because order matters and we don't want to send reportSuites when something is
+ * disabled.
+ * This function does that.
+ *
+ * @param {Object} content
+ * @param {string} key
+ * @returns {(updates: Object) => void}
+ */
+const createMergeConfigOverride = (content, key) => (updates) => {
+  const propertyPath = key.split(".");
+  const hostObjectForUpdates = propertyPath.reduce((obj, propertyName) => {
+    obj[propertyName] = obj[propertyName] || {};
+    return obj[propertyName];
+  }, content);
+
+  Object.assign(hostObjectForUpdates, updates);
+};
+
 // This provides the base functionality that all types of
 // request payloads share.
 export default (options) => {
   const { content, addIdentity, hasIdentity } = options;
-  const mergeConfigOverride = createMerger(content, "meta.configOverrides");
+  const mergeConfigOverrides = createMergeConfigOverride(
+    content,
+    "meta.configOverrides",
+  );
   return {
     mergeMeta: createMerger(content, "meta"),
     mergeState: createMerger(content, "meta.state"),
     mergeQuery: createMerger(content, "query"),
     mergeConfigOverride: (updates) =>
-      mergeConfigOverride(prepareConfigOverridesForEdge(updates)),
+      mergeConfigOverrides(prepareConfigOverridesForEdge(updates)),
     addIdentity,
     hasIdentity,
     toJSON() {
