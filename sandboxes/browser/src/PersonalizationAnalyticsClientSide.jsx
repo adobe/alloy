0b386b37318089bd704cd355a24caadecd35020b
Serban Stancu <stancu@adobe.com>
2025-10-31 09:13:13 -0600

    New fixes.

--- sandboxes/browser/src/PersonalizationAnalyticsClientSide.jsx
+++ sandboxes/browser/src/PersonalizationAnalyticsClientSide.jsx
@@ -63,7 +63,6 @@ export default function PersonalizationAnalyticsClientSide() {
   }, []);
 
   const match = useMatch(window.location.pathname);
-  console.log(match);
 
   return (
     isAlloyConfigured && (
