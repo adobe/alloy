3745dafbb5981601db6629e647ef2bfc34cb416d
Carter McBride <18412686+carterworks@users.noreply.github.com>
2025-12-16 15:32:30 -0700

    Migrate the "add-license" script to an eslint plugin (#1412)
    * Create a licensing eslint plugin
    
    * Convert license inclusion script to eslint plugin
    
    * Use the license_banner for the eslint plugin.
    
    * Delete old file
    
    * Make rule instead of plugin
    
    * Run the plugin
    
    * Fix files with typos in license header
    
    * Fix a case where there is no new line after shebang
    
    * Add names to tests to make them less noisy

---
+++ scripts/specs/licensePlugin.spec.js
@@ -0,0 +1,111 @@
+/*
+Copyright 2025 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import fs from "fs";
+import path from "path";
+import { RuleTester } from "eslint";
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  beforeEach,
+  describe,
+  it,
+} from "vitest";
+import rule from "../eslint/licenseRule.js";
+
+RuleTester.afterAll = afterAll;
+RuleTester.afterEach = afterEach;
+RuleTester.beforeAll = beforeAll;
+RuleTester.beforeEach = beforeEach;
+RuleTester.describe = describe;
+RuleTester.it = it;
+
+const ruleTester = new RuleTester({
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+});
+
+const LICENSE_BANNER_PATH = path.resolve(process.cwd(), "LICENSE_BANNER");
+const getLicenseBannerBody = () => {
+  const bannerText = fs.readFileSync(LICENSE_BANNER_PATH, "utf-8").trimEnd();
+  const lines = bannerText.split(/\r?\n/);
+  return lines.slice(1).join("\n");
+};
+
+const buildHeader = (year) => `/*
+Copyright ${String(year)} Adobe. All rights reserved.
+${getLicenseBannerBody()}
+*/`;
+
+const HEADER_2000 = buildHeader(2000);
+
+ruleTester.run("license-header", rule, {
+  valid: [
+    {
+      name: "header with import statement",
+      code: `${HEADER_2000}\n\nimport foo from "foo";`,
+      options: [{ year: 2000 }],
+    },
+    {
+      name: "header with shebang",
+      code: `#!/usr/bin/env node\n\n${HEADER_2000}\n\nconsole.log("hi");`,
+      options: [{ year: 2000 }],
+    },
+    // Some historical files omit the blank line after the License URL.
+    {
+      name: "header without blank line after URL",
+      code: `/*
+Copyright 2000 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/\n\nexport default 1;`,
+    },
+  ],
+  invalid: [
+    {
+      name: "missing header with import",
+      code: `import foo from "foo";`,
+      options: [{ year: 2000 }],
+      errors: [{ messageId: "missing" }],
+      output: `${HEADER_2000}\n\nimport foo from "foo";`,
+    },
+    {
+      name: "missing header with eslint comment",
+      code: `/* eslint-disable no-console */\nconsole.log("hi");`,
+      options: [{ year: 2000 }],
+      errors: [{ messageId: "missing" }],
+      output: `${HEADER_2000}\n\n/* eslint-disable no-console */\nconsole.log("hi");`,
+    },
+    {
+      name: "missing header with shebang and code",
+      code: `#!/usr/bin/env node\n\nconsole.log("hi");`,
+      options: [{ year: 2000 }],
+      errors: [{ messageId: "missing" }],
+      output: `#!/usr/bin/env node\n\n${HEADER_2000}\n\nconsole.log("hi");`,
+    },
+    {
+      name: "missing header with shebang only",
+      code: `#!/usr/bin/env node`,
+      options: [{ year: 2000 }],
+      errors: [{ messageId: "missing" }],
+      output: `#!/usr/bin/env node\n\n${HEADER_2000}\n\n`,
+    },
+  ],
+});
