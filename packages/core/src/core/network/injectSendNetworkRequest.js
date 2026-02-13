366cc969797ebf389a6c2506d49f46c691fdf753
Spencer Smith <spencer.smith2012@gmail.com>
2026-02-13 11:37:58 -0700

    Fixed broken integration tests (#1446)
    * Fix these int tests
    
    * Add empty changeset
    
    * Remove unnecessary artifacts of earlier test changes

--- packages/core/src/core/network/injectSendNetworkRequest.js
+++ packages/core/src/core/network/injectSendNetworkRequest.js
@@ -29,6 +29,7 @@ export default ({
     // not recursive (it doesn't call toJSON() on the event objects).
     // Parsing the result of JSON.stringify(), however, gives the
     // fully recursive raw data.
+
     const stringifiedPayload = JSON.stringify(payload);
     const parsedPayload = JSON.parse(stringifiedPayload);
 
